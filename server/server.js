7b55479190ad39ea8d87d0266f411747d6a5a67f
Veronica Briones <vbrioneszc@gmail.com>
2018-07-09 18:38:38 -0700

    initial node server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const api = require('./api/route');
+
+app.listen(port, () => console.log('server has started!'));
+
+app.use((req, res, next) => {
+   console.log('time: ', new Date());
+   next()
+});
+
+app.get('/', (req, res) => {
+    console.log('index accessed');
+    res.send(JSON.stringify({success: true}))
+});
+
+app.use('/api', api);
