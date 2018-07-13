49c8eac6fdf40610f9a616c6c9a1d03741d500f9
Christian <cvernikoff@gmail.com>
2018-07-13 08:59:43 -0700

    ports

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.port || process.env.dev_port || 3000;
 
 const api = require('./api/routes');
 
