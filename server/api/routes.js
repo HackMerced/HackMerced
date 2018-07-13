9ccfa169c999a1acaaad01d58319ae6d287ded5f
Christian <cvernikoff@gmail.com>
2018-07-12 22:36:33 -0700

    testing registration form

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res) => {
+router.get('/', (req, res) => {
     console.log('api accessed');
     res.send(JSON.stringify({success: true, page: 'api'}))
 
 });
+router.post('/users/volunteers', ((req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+}));
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const app = express();
-const port = 8080;
+const path = require('path');
 
-const api = require('./api/route');
+const app = express();
+const port = 3000;
+
+const api = require('./api/routes');
 
 app.listen(port, () => console.log('server has started!'));
 
 app.use((req, res, next) => {
-   console.log('time: ', new Date());
-   next()
+    console.log('time: ', new Date());
+    next()
 });
-
-app.get('/', (req, res) => {
-    console.log('index accessed');
-    res.send(JSON.stringify({success: true}))
-});
-
 app.use('/api', api);
+app.use(express.static(path.resolve(__dirname, 'test_public')));
+app.get('*', (req, res) => {
+    console.log('index accessed');
+    res.sendFile(path.join(__dirname, 'test_public/test_index.html'));
+});
+
