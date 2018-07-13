292f0f24586c092cf73bbb47c3430932ef272e35
Chris DeSoto <cdesoto@ucmerced.edu>
2018-07-12 23:58:23 -0700

    Schema stuff

--- server/schema/mock_schema.sh
+++ server/schema/mock_schema.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# If Commandline Utils Aren't Installed, Exit
 if ! foobar_loc="$(type -p mongoimport)" || [[ -z $foobar_loc ]]; then
 	echo "monogoimport not installed"
 	exit 1
@@ -10,11 +11,29 @@ if ! foobar_loc="$(type -p mongodb-dataset-generator)" || [[ -z $foobar_loc ]]; 
 	exit 1
 fi
 
-mongodb-dataset-generator applicants.json -n 50 -o applicants_mock_data.json
+# Mongodb Cloud Atlas URI
+MONGO_HOST=HackMerced-shard-0/hackmerced-shard-00-00-1za3e.mongodb.net:27017,hackmerced-shard-00-01-1za3e.mongodb.net:27017,hackmerced-shard-00-02-1za3e.mongodb.net:27017
 
-echo "Connecting to mongo cloud:"
+# Generate Dummy Data
+mongodb-dataset-generator hackers.json -n 50 -o hackers_mock_data.json 2> /dev/null
+mongodb-dataset-generator judges.json -n 50 -o judges_mock_data.json 2> /dev/null
+mongodb-dataset-generator volunteers.json -n 50 -o volunteers_mock_data.json 2> /dev/null
+mongodb-dataset-generator sponsors.json -n 50 -o sponsors_mock_data.json 2> /dev/null
+
+# Get Username & Password
+echo "Connecting to mongo cloud: "
 echo "Username: "
-read username
+read USERNAME
 echo "Password: "
-read password
-mongoimport --host "mongodb+srv://hackmerced-1za3e.mongodb.net/test" --username $username --password $password --db hackmerced --collection applicants --file ./applicants_mock_data.json --jsonArray
+read -s PASSWORD
+
+# Upload & Import Dummy Data
+mongoimport --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db users --collection hackers --file ./hackers_mock_data.json --jsonArray --drop
+mongoimport --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db users --collection judges --file ./judges_mock_data.json --jsonArray  --drop
+mongoimport --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db users --collection volunteers --file ./volunteers_mock_data.json --jsonArray --drop
+mongoimport --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db users --collection sponsors --file ./sponsors_mock_data.json --jsonArray --drop
+mongofiles --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db resources put person-icon.png --replace
+mongofiles --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db resources put orange-triangle-banner.jpg --replace
+
+# Clean Up
+rm ./hackers_mock_data.json ./judges_mock_data.json ./volunteers_mock_data.json ./sponsors_mock_data.json
