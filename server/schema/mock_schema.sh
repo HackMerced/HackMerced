8fd68173f43fe74d5eaee3087414d12b39ef2755
Chris DeSoto <cdesoto@ucmerced.edu>
2018-07-14 15:54:42 -0700

    Reworked mock_schema.sh to use only schema.json

--- server/schema/mock_schema.sh
+++ server/schema/mock_schema.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check for the command line argument
+if [ "$1" == "" ]; then
+	echo "Please enter the number of dummy documents you would like to produce."
+		echo "Usage: mock_data.sh <number_of_documents>"
+		exit 1
+fi
+
 # If Commandline Utils Aren't Installed, Exit
 if ! foobar_loc="$(type -p mongoimport)" || [[ -z $foobar_loc ]]; then
 	echo "monogoimport not installed"
@@ -11,14 +18,29 @@ if ! foobar_loc="$(type -p mongodb-dataset-generator)" || [[ -z $foobar_loc ]]; 
 	exit 1
 fi
 
+if ! foobar_loc="$(type -p jq)" || [[ -z $foobar_loc ]]; then
+	echo "jq not installed"
+	exit 1
+fi
+
 # Mongodb Cloud Atlas URI
 MONGO_HOST=HackMerced-shard-0/hackmerced-shard-00-00-1za3e.mongodb.net:27017,hackmerced-shard-00-01-1za3e.mongodb.net:27017,hackmerced-shard-00-02-1za3e.mongodb.net:27017
 
+# Insert the correct number of documents to be produced
+sed -i 's/__SIZE__/'$1'/g' schema.json
+
 # Generate Dummy Data
-mongodb-dataset-generator hackers.json -n 50 -o hackers_mock_data.json 2> /dev/null
-mongodb-dataset-generator judges.json -n 50 -o judges_mock_data.json 2> /dev/null
-mongodb-dataset-generator volunteers.json -n 50 -o volunteers_mock_data.json 2> /dev/null
-mongodb-dataset-generator sponsors.json -n 50 -o sponsors_mock_data.json 2> /dev/null
+mongodb-dataset-generator schema.json -n 1 -o mock_data.json > /dev/null
+
+#mongodb-dataset-generator hackers.json -n 50 -o hackers_mock_data.json 2> /dev/null
+#mongodb-dataset-generator judges.json -n 50 -o judges_mock_data.json 2> /dev/null
+#mongodb-dataset-generator volunteers.json -n 50 -o volunteers_mock_data.json 2> /dev/null
+#mongodb-dataset-generator sponsors.json -n 50 -o sponsors_mock_data.json 2> /dev/null
+
+jq '.[0].hackers' mock_data.json > hackers_mock_data.json
+jq '.[0].judges' mock_data.json > judges_mock_data.json
+jq '.[0].volunteers' mock_data.json > volunteers_mock_data.json
+jq '.[0].sponsors' mock_data.json > sponsors_mock_data.json
 
 # Get Username & Password
 echo "Connecting to mongo cloud: "
@@ -35,5 +57,9 @@ mongoimport --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD -
 mongofiles --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db resources put person-icon.png --replace
 mongofiles --host $MONGO_HOST --ssl --username $USERNAME --password $PASSWORD --authenticationDatabase admin --db resources put orange-triangle-banner.jpg --replace
 
+# Replace the placeholder for document size
+sed -i 's/{"size": '$1'/{"size": __SIZE__/g' schema.json
+
 # Clean Up
-rm ./hackers_mock_data.json ./judges_mock_data.json ./volunteers_mock_data.json ./sponsors_mock_data.json
+rm ./mock_data.json ./hackers_mock_data.json ./judges_mock_data.json ./volunteers_mock_data.json ./sponsors_mock_data.json
+
