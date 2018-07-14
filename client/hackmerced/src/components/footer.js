d651cf6c8e075ae57c3697d5c3e1af41147a5ed2
Devanshu Kumar <devanshu.kumar07@gmail.com>
2018-07-14 10:32:56 -0700

    adding scss and react files for the title screen

---
+++ client/hackmerced/src/components/footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+      <nav>
+        <ul className="listfoot">
+          <li>
+            <a href="#">Contact Us</a>
+
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">FAQs</a>
+          </li>
+        </ul>
+      </nav>
+      </footer>
+    );
+  }
+}
+
+export default Footer ;
