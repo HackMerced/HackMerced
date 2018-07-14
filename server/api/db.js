0207ac50d1d304344852c35eba1755883922d8af
Christian <cvernikoff@gmail.com>
2018-07-13 20:22:25 -0700

    async db_stuff

---
+++ server/api/db.js
@@ -0,0 +1,15 @@
+const mongoDB = require('mongodb').MongoClient;
+const url = 'mongodb+srv://' + (process.env.db_username || 'username') + ':' + (process.env.db_password || 'password') + '@hackmerced-1za3e.mongodb.net';
+const connection = mongoDB.connect(url, {useNewUrlParser: true}).catch((e) => {
+    console.log(e.message, "MONGODB");
+});
+const users = async () => {
+    const completed_connection = await connection;
+    return await completed_connection.db('users');
+};
+const resources = async () => {
+    const completed_connection = await connection;
+    return await completed_connection.db('resources');
+};
+module.exports.users = users;
+module.exports.resources = resources;
--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.port || process.env.dev_port || 3000;
+const port = process.env.port || 3000;
 
 const api = require('./api/routes');
+const mongo = require('./api/db');
 
 app.listen(port, () => console.log('server has started!'));
 
@@ -17,7 +18,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api', api);
 app.use(express.static(path.resolve(__dirname, 'test_public')));
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
+    const users = await mongo.users();
+    const user_data = await users.collection('judges').find().toArray();
+    console.log(user_data);
     console.log('index accessed');
     res.sendFile(path.join(__dirname, 'test_public/test_index.html'));
 });
