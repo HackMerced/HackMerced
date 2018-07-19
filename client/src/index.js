ac1b838be54c7e91a6dc4e57fb62c819d3da668e
kechuarceus <jesus2sanchezmacias@gmail.com>
2018-07-18 23:51:23 -0700

    finished aboutUs and team components and npm bulma

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AboutUS from "./layouts/aboutUS"
+import Team from "./layouts/team"
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
 registerServiceWorker();
