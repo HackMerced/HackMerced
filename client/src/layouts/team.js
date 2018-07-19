8a02759b40338f0fd1f672cc23f2cbefe376f4db
kechuarceus <jesus2sanchezmacias@gmail.com>
2018-07-18 23:51:23 -0700

    finished aboutUs and team components and npm bulma

--- client/src/layouts/team.js
+++ client/src/layouts/team.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import "../assets/css/team.css";
+import "../assets/css/aboutUS.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import 'bulma/css/bulma.css'
 
-class aboutUS extends Component {
-    render() {
-      return (
-          <div className = "aboutUS">
-            <Header/>
-            
-            
-            <div class = "columns">
+class team extends Component{
+    render(){
+        return(
+            <div className = "team">
+                <Header/>
+                <div class = "columns">
                 <div class = "column is-2">
                     
                 </div>
                 <div class = "column is-8">
-                    <h1 class = "title">About Us</h1>  
-                    <hr/>               
+                    <h1 class = "title">Meet The Team </h1>
+                    <hr/>                 
                     <div class = "box">
                            ANY CONTENT WILL GO HERE
                            <br/>
@@ -40,10 +38,13 @@ class aboutUS extends Component {
     
       
 
-            <Footer/>
-        </div>
-      );
+                <Footer/>
+             </div>
+
+        );
+
     }
-  }
-  
-  export default aboutUS;
+
+}
+
+export default team;
