677709e17b970b37712ea3d95eae1545762041fd
Chris DeSoto <cdesoto22@yahoo.com>
2018-07-22 22:49:38 -0700

    Cleaning up code, fixing comments and logging

--- deployment/hmd.sh
+++ deployment/hmd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Useful functions
 get_unix_timestamp() {
 	echo "$(date +'%s')"
@@ -15,8 +14,7 @@ die() {
 }
 # Check for branch name in argunment
 if [[ ! $BRANCH ]]; then
-	log "Branch not specified in env vars"
-	die
+	log "Branch not specified in env vars" && die
 fi
 log "Running hackmerced deployment script on branch: $BRANCH"
 # While lock is not held
@@ -26,8 +24,7 @@ while [ -f /root/hmd.lock ]; do
 	CURR_TIME=$(get_unix_timestamp)
 	# Check for timeout
 	if [ $(( CURR_TIME - START_TIME )) -gt 480 ]; then
-		log "Timeout occured while waiting for the process to obtain the lock file"
-		die
+		log "Timeout occured while waiting for a previous instance of the process complete or the lock file needs to be reset" && die
 	fi
 done
 # Lock is available and timeout has not occured, acquire it
@@ -45,9 +42,9 @@ mkdir $REPO_LOC && log "Creating new repo folder"
 # Clone new repo files
 git clone -b $BRANCH git@github.com:HackMerced/HackMercedf18.git $REPO_LOC && log "Cloning new repo to $REPO_LOC"
 # Install node dependencies
-npm --prefix $REPO_LOC_SERVER install $REPO_LOC_SERVER && log "Installing node server dependencies"
+npm --prefix $REPO_LOC_SERVER install $REPO_LOC_SERVER && log "Installing server node dependencies"
 if [ -e "$REPO_LOC_CLIENT/package.json" ]; then
-	npm --prefix $REPO_LOC_CLIENT run build $REPO_LOC_CLIENT && log "Installing node client dependencies"
+	npm --prefix $REPO_LOC_CLIENT run build $REPO_LOC_CLIENT && log "Installing client node dependencies"
 fi
 # Start pm2
 env HOME=$PM2_ENV pm2 start $PROC --update-env && log "Restarting pm2 $PROC processs"
