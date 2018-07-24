1bf678652919d2dbd8de914b1204aa55ae5e3349
Chris DeSoto <cdesoto22@yahoo.com>
2018-07-23 19:18:11 -0700

    More reworking, cleaning up code

--- deployment/hmd.sh
+++ deployment/hmd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#
+# HackMerced Deployment Script
+#
+
 # Useful functions
 get_unix_timestamp() {
 	echo "$(date +'%s')"
@@ -17,14 +22,24 @@ die() {
 	log e "Script failed"
 	exit 0
 }
+
 # Check for branch name in argunment
 if [[ ! $BRANCH ]]; then
 	log i "Branch not specified in env vars" && die
 fi
+
 log i "START -- Running hackmerced deployment script on branch: $BRANCH"
+
+# Set up some vars
+PROC="hackmerced_$BRANCH"
+REPO_LOC="/opt/$PROC"
+REPO_LOC_SERVER="$REPO_LOC/server/"
+REPO_LOC_CLIENT="$REPO_LOC/client/"
+HMD_LOCK="$REPO_LOC/hmd.lock"
+
 # While lock is not held
 START_TIME=$(get_unix_timestamp)
-while [ -f /home/pm2/hmd.lock ]; do
+while [ -f $HMD_LOCK ]; do
 	sleep 120
 	CURR_TIME=$(get_unix_timestamp)
 	# Check for timeout
@@ -32,23 +47,27 @@ while [ -f /home/pm2/hmd.lock ]; do
 		log e "Timeout occured while waiting for a previous instance of the process complete or the lock file needs to be reset" && die
 	fi
 done
+
 # Lock is available and timeout has not occured, acquire it
-touch /home/pm2/hmd.lock && log i "Acquiring lock file.."
-# Set up some vars
-PROC="hackmerced_$BRANCH"
-REPO_LOC="/opt/$PROC"
-REPO_LOC_SERVER="$REPO_LOC/server/"
-REPO_LOC_CLIENT="$REPO_LOC/client/"
+touch $HMD_LOCK && log i "Acquiring lock file.."
+
 # Stop pm2
 env HOME=$PM2_ENV pm2 stop $PROC && log i "Stopping pm2 $PROC process"
+
 # Remove old repo files
 rm -rf "$REPO_LOC/{*,.*}" && log i "Clearing old repo files"
+
 # Clone new repo files
 git clone -b $BRANCH git@github.com:HackMerced/HackMercedf18.git $REPO_LOC && log i "Cloning new repo to $REPO_LOC"
+
 # Install node dependencies
 npm --prefix $REPO_LOC_SERVER install $REPO_LOC_SERVER && log i "Installing server node dependencies"
 npm --prefix $REPO_LOC_CLIENT run build $REPO_LOC_CLIENT && log i "Installing client node dependencies"
+
 # Start pm2
 env HOME=$PM2_ENV pm2 start $PROC --update-env && log i "Restarting pm2 $PROC processs"
+
 # Free lock file
-rm /home/pm2/hmd.lock && log i "Freeing lock file.." && log i "END -- Repo update complete"
+rm $HMD_LOCK && log i "Freeing lock file.."
+
+log i "END -- Repo update complete"
