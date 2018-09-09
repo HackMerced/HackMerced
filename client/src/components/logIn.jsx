1503caebba8703611d56760fe708d821dadf9d99
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-09-09 13:44:49 -0700

    made apply form and volunteer form prettier. Added handle submit functions to login form

--- client/src/components/logIn.jsx
+++ client/src/components/logIn.jsx
@@ -3,7 +3,24 @@ import logo from "../assets/img/logo.png";
 var FontAwesome = require('react-fontawesome');
 
 class logIn extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            myEmail: '',
+            myPassword: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(EventTarget) {
+       this.setState({ [EventTarget.target.name] : EventTarget.target.value});
+    }
+
+    handleSubmit(EventTarget) {
+        alert('LOGGED IN!!!');
+        EventTarget.preventDefault();
+    }
     render() { 
         return ( 
             <section className = "hero is-primary is-fullheight">
@@ -11,7 +28,7 @@ class logIn extends Component {
                     <div className = "container">
                         <div className = "columns is-centered">
                             <div className = "column is-4">
-                                <form className = "box">
+                                <form className = "box" onSubmit={this.handleSubmit}>
                                     <div className = "field has-text-centered">
                                         <img src = {logo} style ={{height: 200, width: 200}}/>
                                         
@@ -20,7 +37,7 @@ class logIn extends Component {
                                     <div className = "field">
                                         <label>Email</label>
                                             <div className = "control has-icons-left">
-                                                <input name = "myemail" className = "input" type = "email" placeholder = "Enter Email"/>
+                                                <input name = "myEmail" className = "input" type = "email" placeholder = "Enter Email"/>
                                                     <span className = "icon is-small is-left">
                                                         <FontAwesome className = "fa-envelope"/>
                                                     </span>
@@ -30,7 +47,7 @@ class logIn extends Component {
                                     <div className = "field">
                                         <label>Password</label>
                                             <div className = "control has-icons-left">
-                                                <input name = "mypassword" className = "input" type = "password" placeholder = "Enter Password"/>
+                                                <input name = "myPassword" className = "input" type = "password" placeholder = "Enter Password"/>
                                                     <span className = "icon is-small is-left">
                                                         <FontAwesome className = "fa-lock"/>
                                                     </span>
@@ -38,7 +55,7 @@ class logIn extends Component {
                                     </div>
 
                                     <div className = "field">
-                                        <button className = "button is-info is-outlined">
+                                        <button className = "button is-info is-outlined" type="submit" value = "Submit">
                                             Login
                                         </button>
                                     
