027278c81d50c76e0a9f5df1b30b143e6ba124f6
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-09-09 13:44:49 -0700

    made apply form and volunteer form prettier. Added handle submit functions to login form

--- client/src/components/signUPVolunteer.jsx
+++ client/src/components/signUPVolunteer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "../assets/css/signUp.css";
 
 class Volunteer extends Component {
     constructor(props) {
@@ -28,45 +29,57 @@ class Volunteer extends Component {
     render() {
         return (
         <signup>
-            <h2>Volunteer Sign-Up Form</h2>
-            <form onSubmit={this.handleSubmit}>
-            <label>
-                Name:
-                <input name="myName" type="text" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label>
-                Email:
-                <input name="myEmail" type="text" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label>
-                Phone Number:
-                <input name="myPhone" type="text" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label>
-                School:
-                <input name="mySchool" type="text" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label> 
-                Age:
-                <input name="myAge" type="number" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label>
-                Gender:
-                <input name="myGender" type="text" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label>
-                Shirt Size:
-                <input name="myShirt" type="text" onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <input type="submit" value = "Submit" />
-            </form>
+
+            <center><span className = "title1">Volunteer Sign-Up Form</span></center>
+            <br/>
+            <div className = "columns is-centered">
+                <div className = "column is-4-desktop">
+                    <form className = "box" onSubmit={this.handleSubmit}>
+
+                        <div className = "field">
+                            <label className = "label"> Name: </label>
+                            <input className = "input"name="myName" type="text" onChange={this.handleInputChange} />
+                        </div>
+
+                        <div className = "field">
+                            <label className = "label">Email: </label>
+                            <input className = "input" name="myEmail" type="text" onChange={this.handleInputChange} />
+                        </div>
+
+                        <div className = "field">
+                            <label className = "label">Phone Number: </label> 
+                            <input className = "input" name="myPhone" type="text" onChange={this.handleInputChange} />
+                        </div>
+
+                        <div className = "field">
+                            <label className = "label">School:</label>
+                            <input className = "input" name="mySchool" type="text" onChange={this.handleInputChange} />
+                        </div>
+             
+                        <div className = "columns">
+                            <div className = "column -6">
+                                <div className = "field">
+                                    <label className = "label">Age:</label> 
+                                    <input className = "input" name="myAge" type="number" onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            <div className = "column is-6">
+                                <div className = "field">
+                                    <label className = "label">Gender:</label>
+                                    <input className = "input" name="myGender" type="text" onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className  ="field">
+                            <label className = "label">Shirt Size: </label>
+                            <input className = "input" name="myShirt" type="text" onChange={this.handleInputChange} />
+                        </div>
+
+                        <input className = "button is-success" type="submit" value = "Submit" />
+                    </form>
+                </div>
+            </div>
         </signup>
         );
     }
