22f2bf7259a9fb2424207c48ba529f17762f65ca
Chris DeSoto <cdesoto@ucmerced.edu>
2018-09-23 23:43:59 -0700

    Cleaned up shell script a bit. Fixed bug with truthy/falsy input.

--- server/model/mock_data.sh
+++ server/model/mock_data.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
+#
+# Get vars and run mock_data.js
+#
 
-# Username & Password
-echo -n 'DB Username: '
-read db_username
+# Username & Password to MongoDB
+read -p 'DB Username: ' db_username
 export db_username
-echo -n 'DB Password: '
-read -s db_password
+read -p 'DB Password: ' -s db_password
 export db_password
 echo ''
 
-# Drop Collections
-echo -n 'Drop collections (y/n): '
-read DROP_COLLECTIONS
-DROP_COLLECTIONS=$(echo $DROP_COLLECTIONS | awk '{print tolower($0)}')
-if [[ $DROP_COLLECTIONS == 'y' || $DROP_COLLECTIONS == 'yes' ]]; then
-    echo -n 'Are you sure? This will drop all collections on the db (y/n): '
-    read DROP_COLLECTIONS
+# Ask to drop collections. If no, var is set to empty string for falsy comparison.
+read -p 'Drop collections (y/n): ' DROP_COLLECTIONS
+if [[ $DROP_COLLECTIONS =~ ^([yY](es|ES){0,2})$ ]]; then
+  read -p 'Are you sure? This will drop all collections on the db (y/n): ' DROP_COLLECTIONS
+  if [[ ! $DROP_COLLECTIONS =~ ^([yY](es|ES){0,2})$ ]]; then
+    DROP_COLLECTIONS=''
+  fi
+else
+  DROP_COLLECTIONS=''
 fi
-DROP_COLLECTIONS=$(echo $DROP_COLLECTIONS | awk '{print tolower($0)}')
 export DROP_COLLECTIONS
 
-# Mock Data Size
-echo -n 'The number of mock data entries will default to 50. Specify other mock data size (y/n): '
-read CUSTOM_MOCK_SIZE
+# Ask to set mock data size. Default is 50.
+read -p 'The number of mock data entries will default to 50. Specify other mock data size (y/n): ' CUSTOM_MOCK_SIZE
 CUSTOM_MOCK_SIZE=$(echo $CUSTOM_MOCK_SIZE | awk '{print tolower($0)}')
-if [[ $CUSTOM_MOCK_SIZE == 'y' || $CUSTOM_MOCK_SIZE == 'yes' ]]; then
-    echo -n 'Size: '
-    read MOCK_SIZE
-    export MOCK_SIZE
+if [[ $CUSTOM_MOCK_SIZE =~ ^([yY](es|ES){0,2})$ ]]; then
+  read -p 'Size: ' MOCK_SIZE
+  export MOCK_SIZE
 fi
 
 # Start mock_data node script
