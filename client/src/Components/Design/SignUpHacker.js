235145a48eaa2b5b467306b5925882c6387d1698
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-09-30 10:42:18 -0700

    added axios and semantic UI. Made endpoint for signUpHacker form

--- client/src/Components/Design/SignUpHacker.js
+++ client/src/Components/Design/SignUpHacker.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import "../../assets/css/signUp.css";
 
-// var FontAwesome = require('react-fontawesome');
+//IMPORT AXIOS
+import axios from "axios";
+
+//IMPORT SEMANTIC-UI
+import {Container, Button, Form, Select} from "semantic-ui-react";
+
+//VALUES FOR SHIRTS
+const options = [
+  {key: '1', text: 'Small', value : 'small'},
+  {key: '2', text: 'Medium', value : 'medium'},
+  {key: '3', text: 'Large', value : 'large'},
+  {key: '4', text: 'X-Large', value : 'X-Large'}
+]
 
 class SignUpHacker extends Component {
   constructor(props) {
@@ -28,55 +40,72 @@ class SignUpHacker extends Component {
   handleSubmit(EventTarget) {
     alert('Your application was submitted!');
     EventTarget.preventDefault();
+
+    const user = {
+      myName : this.state.myName,
+      myEmail : this.state.myEmail,
+      myPhone : this.state.myPhone,
+      mySchool : this.state.mySchool,
+      myAge : this.state.myAge,
+      myGender : this.state.myGender,
+      myShirt : this.state.myShirt,
+      myDiet : this.state.myDiet,
+      myAllergies : this.state.myAllergies
+    };
+
+    axios.post("http://localhost:3000/", {user})
+    .then(res =>{
+      console.log(res.data);
+    })
   }
 
   render() {
     return (
-      <signup>
-        <h2>Hacker Sign-Up Form</h2>
-        <div className="columns is-centered">
-          <div className="column is-4-desktop">
-            <form class="box" onSubmit={this.handleSubmit}>
-              <div className="field">
-                <label className="label" >Name: </label>
-                <input className="inputh" name="myName" type="text" onChange={this.handleInputChange} />
-              </div>
-              <div className="field">
-                <label className="label">Email: </label>
-                <input className="inputh" name="myEmail" type="text" onChange={this.handleInputChange} />
-              </div>
-              <div className="field">
-                <label className="label">Phone Number: </label>
-                <input className="inputh" name="myPhone" type="text" onChange={this.handleInputChange} />
-              </div>
-              <div className="field">
-                <label className="label">School: </label>
-                <input className="inputh" name="mySchool" type="text" onChange={this.handleInputChange} />
-              </div>
-              <div className="field">
-                <label className="label">Age: </label>
-                <input className="inputh" name="myAge" type="number" onChange={this.handleInputChange} />
-              </div> 
-              <div className="field">
-                <label className="label">Gender: </label>
-                <input className="inputh" name="myGender" type="text" onChange={this.handleInputChange} />
-              </div>
-                <label className="label">Shirt Size: </label>
-                <input className="inputh" name="myShirt" type="text" onChange={this.handleInputChange} />
-                <label className="label">Dietary Restrictions: </label>
-                <input className="inputh" name="myDiet" type="text" onChange={this.handleInputChange} />  
-              <label className="label">Allergies </label>
-                <input className="inputh" name="myAllergies" type="text" onChange={this.handleInputChange} />
-              <label>
-                Submit Your Resume
-                <input type="file" />
-              </label>
-              <br />
-              <input className="button is-success" type="submit" value="Submit" />
-            </form>
-          </div>
-        </div>
-      </signup>
+        <Container text>
+          <center><h2>Hacker Sign-Up Form</h2></center>
+          <Form onSubmit={this.handleSubmit}>
+                <Form.Field>
+                  <label className="label" >Name: </label>
+                  <input  name="myName" type="text" onChange={this.handleInputChange} />
+                </Form.Field>
+                <Form.Field>
+                  <label className="label">Email: </label>
+                  <input  name="myEmail" type="text" onChange={this.handleInputChange} />
+                </Form.Field>
+                <Form.Field>
+                  <label className="label">Phone Number: </label>
+                  <input  name="myPhone" type="text" onChange={this.handleInputChange} />
+                </Form.Field>
+                <Form.Field>
+                  <label className="label">School: </label>
+                  <input  name="mySchool" type="text" onChange={this.handleInputChange} />
+                </Form.Field>
+
+                <Form.Group widths = 'equal'>
+                  <Form.Field>
+                    <label className="label">Age: </label>
+                    <input  name="myAge" type="text" onChange={this.handleInputChange} />
+                  </Form.Field>
+
+                  <Form.Field>
+                    <label className="label">Gender: </label>
+                    <input  name="myGender" type="text" onChange={this.handleInputChange} />
+                  </Form.Field>
+
+                  <Form.Field control = {Select} options = {options} label = "Shirt Size:"/>
+                </Form.Group>
+                  <label className="label">Dietary Restrictions: </label>
+                  <input  name="myDiet" type="text" onChange={this.handleInputChange} />  
+                <label className="label">Allergies </label>
+                  <input  name="myAllergies" type="text" onChange={this.handleInputChange} />
+                <label>
+                  Submit Your Resume
+                  <input type="file" />
+                </label>
+                <br />
+                <input className="button is-success" type="submit" value="Submit" />
+          </Form>
+          </Container>
     );
   }
 }
