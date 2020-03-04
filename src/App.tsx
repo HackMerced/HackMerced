3a4f9a4cd7c9632cd301a351cc7b21e1c3071627
Adrian Darian <adarian@ucmerced.edu>
2020-03-04 15:22:52 -0800

    Setup Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <div className="m-auto antialiased font-sans font-serif font-mono text-center">
+            <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="text-blue-300" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn Taiwlind with React TypeScript
+                </a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [
+        ({ addComponents }) => {
+            const AppLogo = {
+                '.App-logo': {
+                    animation: 'App-logo-spin infinite 20s linear',
+                    height: '40vmin',
+                    'pointer-events': 'none',
+                },
+                '@keyframes App-logo-spin': {
+                    from: {
+                        transform: 'rotate(0deg)',
+                    },
+                    to: {
+                        transform: 'rotate(360deg)',
+                    },
+                },
+            };
+            addComponents(AppLogo);
+        },
+    ],
+};
