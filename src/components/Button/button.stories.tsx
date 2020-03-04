2065b73fd9fa372aa2c853d8f149fdb2bd2aa605
Adrian Darian <adarian@ucmerced.edu>
2020-03-04 11:44:04 -0800

    Renamed to follow best practices @shub

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,7 +1,7 @@
 import { checkA11y } from '@storybook/addon-a11y';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
-import Button from '.';
+import Button from './Button';
 
 storiesOf('Button', module)
     .addDecorator(checkA11y)
