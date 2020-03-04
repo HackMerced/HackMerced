a8fbeb47edc2003d1ce93d18fa50c3c37b046128
Adrian Darian <adarian@ucmerced.edu>
2020-03-04 13:38:22 -0800

    Added a more suitable storybook environment

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,23 @@
-import { configure } from '@storybook/react';
-import { checkA11y } from '@storybook/addon-a11y';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
+import centered from '@storybook/addon-centered/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-addDecorator(checkA11y);
+addDecorator(withA11y);
+addDecorator(centered);
+addDecorator(withKnobs);
+addParameters({
+    docs: {
+        container: DocsContainer,
+        page: DocsPage,
+    },
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+        defaultViewport: 'responsive',
+    },
+});
 
 const req = require.context('../src/components', true, /.stories.tsx$/);
 
