efab33abac8b47e23b4459cc63904a1d2fedec28
Adrian Darian <adarian@ucmerced.edu>
2020-03-04 16:29:10 -0800

    Jest and Enzyme Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+configure({ adapter: new EnzymeAdapter() });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
