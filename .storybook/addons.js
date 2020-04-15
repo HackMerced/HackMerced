38ce904bd8b763760a9a7d1148b8fada4c012740
Adrian Darian <adarian@ucmerced.edu>
2020-04-14 20:39:18 -0700

    Updates

--- .storybook/addons.js
+++
@@ -1,7 +0,0 @@
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-docs/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-centered/react';
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-viewport/register';
-import '@storybook/addon-notes/register';
---
+++ precache-manifest.4d8eacb276648487e7ab7c907465a3da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a84000398e2c77024a13343abe37df9",
+    "url": "/HackMerced/index.html"
+  },
+  {
+    "revision": "7ff968de29b6e2a93369",
+    "url": "/HackMerced/static/css/main.f19f92cc.chunk.css"
+  },
+  {
+    "revision": "96b982ef57a767d6e03d",
+    "url": "/HackMerced/static/js/2.57677e1e.chunk.js"
+  },
+  {
+    "revision": "c760b28870e57f556222165aa4da0484",
+    "url": "/HackMerced/static/js/2.57677e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff968de29b6e2a93369",
+    "url": "/HackMerced/static/js/main.65bff9d3.chunk.js"
+  },
+  {
+    "revision": "0ed588c49ad7804309b3",
+    "url": "/HackMerced/static/js/runtime-main.96d7a6b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HackMerced/static/media/logo.5d5d9eef.svg"
+  }
+]);
