caf4adf2009b3e1c2a39ca65f2cb153bacbcf240
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-05-21 00:01:03 -0700

    created navbar

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,17 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MLHBANNER from '../../assets/images/mlh-badge.svg';
+
 import './navbar.scss';
-import HMLOGO from '../../assets/images/hackmerced-logo.png';
 
 const styles = {
     width: '100px',
-    height: '40px',
+    height: '175px',
 };
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="NAVBAR">
-            <div className="hackmerced-logo">
-                <img src={HMLOGO} style={styles} alt="HACKMERCED LOGO" />
+        <nav className="NAVBAR sticky flex">
+            <div className="NAVTEXT block flex-auto ">
+                <ul className="flex float-right">
+                    <li className="mr-3 active">
+                        <Link
+                            className="active inline-block rounded font-bold hover:text-black hover:bg-white text-gray-100 py-2 px-3"
+                            to="/"
+                        >
+                            HOME
+                        </Link>
+                    </li>
+                    <li className="mr-3">
+                        <a
+                            className="inline-block rounded font-bold hover:text-black hover:bg-white  py-2 px-3"
+                            href="http://localhost:3000/"
+                        >
+                            EVENTS
+                        </a>
+                    </li>
+                    <li className="mr-3">
+                        <a
+                            className="inline-block rounded font-bold hover:text-black hover:bg-white py-2 px-3 text-gray-100 "
+                            href="http://localhost:3000/"
+                        >
+                            PAST HACKATHONS
+                        </a>
+                    </li>
+                    <li className="mr-3">
+                        <a
+                            className="inline-block rounded font-bold hover:text-black hover:bg-white py-2 px-3 text-gray-100 "
+                            href="http://localhost:3000/"
+                        >
+                            SPONSOR US
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="inline-block rounded font-bold hover:text-black hover:bg-white py-2 px-3 text-gray-100 "
+                            href="http://localhost:3000/"
+                        >
+                            CONTACT US
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="MLH-banner">
+                <img src={MLHBANNER} style={styles} alt="MLH BANNER" />
             </div>
         </nav>
     );
