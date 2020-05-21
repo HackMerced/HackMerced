f232ae16df6deae2905699d511b24e817d7b5bd6
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-05-21 00:00:39 -0700

    created title component

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import HMLOGO from '../../assets/images/hackmerced-logo.png';
+
 import './title.scss';
+
+const styles = {
+    width: '100px',
+    height: '90px',
+};
 
 const Title: React.FC = () => {
     return (
         <section className="TITLE">
-            <div className="hackmerced-logo"></div>
+            <div className="hackmerced-logo">
+                <Link to="/">
+                    {' '}
+                    <img src={HMLOGO} style={styles} alt="HACKMERCED LOGO" />
+                </Link>
+            </div>
+            <div className="font-black TITLE-TEXT">
+                HackMerced <br />{' '}
+            </div>
+            <div className="title-subtext">The biggest San Joaquin Valley hackathon.</div>
+            <button className="bg-white text-black BUTTON font-bold">Subscribe</button>
         </section>
     );
 };
