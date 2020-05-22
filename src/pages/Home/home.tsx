44a11956ba5bc8a101df5038f116c7d5cee8155f
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-05-21 21:33:48 -0700

    created navbar

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Title from '../../components/Title/title';
 import Navbar from '../../components/NavBar/navbar';
+import Footer from '../../components/Footer/footer';
 
 const Home: React.FC = () => {
     return (
         <div className="">
             <Navbar />
             <Title />
+            <Footer />
         </div>
     );
 };
