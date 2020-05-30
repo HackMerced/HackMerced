6797ec440fe876b999b39a76f996596b7e067a56
Truman Chan <trumanjfchan@gmail.com>
2020-05-29 18:53:02 -0700

    Created Team Pics component for Home-page
    The code is not responsive, so I will / we will have to change it later on.

---
+++ src/components/TeamPics/teampics.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import './teampics.scss';
+
+
+const TeamPics: React.FC = () => {
+    return (
+        <div className="Component">
+
+            <div className="Header">
+                <h1>Our Team</h1>
+            </div>
+
+            <div className="Container1">
+                <div className="Card1">
+                    <a href="https://www.linkedin.com/in/trumanjfchan/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQEMNlFOLpEXOg/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=q2e8-Rv_opaZldSkg5Ev0OO7KV7E2WChQ3AF-vQSNFk" alt="Truman" />
+                    </a>
+                </div>
+                <div className="Card1">
+                    <a href="https://www.linkedin.com/in/lorenzo-scaturchio-62203b160/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQHIbAw3aAk5dg/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=loF4Q6sFxwsqn_7CKU0jDYHOiF7IAAZeqlSbVtpYr6g" alt="Lorenzo" />
+                    </a>
+                </div>
+                <div className="Card1">
+                    <a href="https://www.linkedin.com/in/maryfrancine/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQFP5quFVaGqcQ/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=1tQ5qPzNxGIGZ3jxHo-uwoWGzz8LltKCflMVDS5Prsg" alt="Mary" />
+                    </a>
+                </div>
+                <div className="Card1">
+                    <a href="https://www.linkedin.com/in/shiv248/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQFaDSfdUHo1gg/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=dBvv2phu7MsK2jtILFw7PPv_GIgZcJxFFsVBaLmqw9Q" alt="Shiv" />
+                    </a>
+                </div>
+                <div className="Card1">
+                    <a href="https://www.linkedin.com/in/athena-siaotong/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQH9zr_CZIVMAQ/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=kptUKgOIPzvG3Elu-EDADdL1i0HgJJpkP9M6CPA5YBk" alt="Athena" />
+                    </a>
+                </div>
+                <div className="Card1">
+                    <a href="https://www.linkedin.com/in/adriandarian/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQGeZi-Ciku94A/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=gqZD6augIL_JI8bXt-_bx5AnihwaLINd5mJq1_ktNJM" alt="Adrian" />
+                    </a>
+                </div>
+            </div>
+
+            <div className="Container2">
+                <div className="Card2">
+                    <a href="https://www.linkedin.com/in/busher-bridi/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQHdm42_yBgoUw/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=kftsgXhyADNOcJzcWDQegbeswnhKQ2P_PPv0YOor_vs" alt="Busher" />
+                    </a>
+                </div>
+                <div className="Card2">
+                    <a href="https://www.linkedin.com/in/tuzette-vu-500550100/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQFvhoAkLHRr1Q/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=LyY4lBRCUWSO9C8Md49hQVCqAi3LnYVQFXdeYAya_Lo" alt="Tuzette" />
+                    </a>
+                </div>
+                <div className="Card2">
+                    <a href="https://www.linkedin.com/in/manjot-singh-0a41a8195/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQGnwXogrvm77A/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=YoldPpq9kb8HxZkvzwflxqH66AV2NdgUf_t1BvAai-A" alt="Manjot" />
+                    </a>
+                </div>
+                <div className="Card2">
+                    <a href="https://www.linkedin.com/in/kacey-su-351950176/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQE_tI2CyMuGOA/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=CEd--ADpJO7tdZoEB5jkGJJzpo4-tVRQUzD7diQfyMQ" alt="Kacey" />
+                    </a>
+                </div>
+                <div className="Card2">
+                    <a href="https://www.linkedin.com/in/sidharth-babu/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4D03AQG-wdMtjaAf-w/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=kAGm6x-RdyKXQkObqYiN7bKRt9TyLFh5zryFRQnWI3g" alt="Sid" />
+                    </a>
+                </div>
+                <div className="Card2">
+                    <a href="https://www.linkedin.com/in/intiseraziz/">
+                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQFM85P-XiR-aQ/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=S3Q9hdinguD7DC9aJHYbmZYb9fERKmVaQ0_Dna93bBo" alt="Intiser" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TeamPics;
