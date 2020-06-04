7d7a6e99e2190df32b66d2108ea8e38d9af15759
Truman Chan <trumanjfchan@gmail.com>
2020-06-03 22:12:22 -0700

    Made the TeamPics Component Responsive!
    I used a different source online that had code for a responsive hexagon design, and edited it. I also added media queries for the Header of the component, "Our Team", so the font-size changes based on a specified resolution's width. We also decided not to use the linkedIn photos because the default resolution on those are too small, and would need to be scaled. For now, there are placeholders because members need to upload their photos into our Google Drive.

--- src/components/TeamPics/teampics.tsx
+++ src/components/TeamPics/teampics.tsx
@@ -1,83 +1,119 @@
 import React from 'react';
 
+import TRUMAN from '../../assets/images/truman.jpg';
+import LORENZO from '../../assets/images/lorenzo.jpg';
+import MARY from '../../assets/images/mary.jpg';
+//import SHIV from '../../assets/images/shiv.jpg';
+//import ATHENA from '../../assets/images/athena.jpg';
+//import ADRIAN from '../../assets/images/adrian.jpg';
+//import BUSHER from '../../assets/images/busher.jpg';
+//import TUZETTE from '../../assets/images/tuzette.jpg';
+//import MANJOT from '../../assets/images/manjot.jpg';
+//import KACEY from '../../assets/images/kacey.jpg';
+//import SID from '../../assets/images/sid.jpg';
+//import INTISER from '../../assets/images/intiser.jpg';
+
 import './teampics.scss';
 
 
-const TeamPics: React.FC = () => {
-    return (
-        <div className="Component">
+/* Outline for this component largely taken from: https://www.codesmite.com/article/how-to-create-pure-css-hexagonal-grids */
 
+
+const Test: React.FC = () => {
+    return (
+        <body>
             <div className="Header">
                 <h1>Our Team</h1>
             </div>
 
-            <div className="Container1">
-                <div className="Card1">
-                    <a href="https://www.linkedin.com/in/trumanjfchan/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQEMNlFOLpEXOg/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=q2e8-Rv_opaZldSkg5Ev0OO7KV7E2WChQ3AF-vQSNFk" alt="Truman" />
-                    </a>
-                </div>
-                <div className="Card1">
-                    <a href="https://www.linkedin.com/in/lorenzo-scaturchio-62203b160/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQHIbAw3aAk5dg/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=loF4Q6sFxwsqn_7CKU0jDYHOiF7IAAZeqlSbVtpYr6g" alt="Lorenzo" />
-                    </a>
-                </div>
-                <div className="Card1">
-                    <a href="https://www.linkedin.com/in/maryfrancine/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQFP5quFVaGqcQ/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=1tQ5qPzNxGIGZ3jxHo-uwoWGzz8LltKCflMVDS5Prsg" alt="Mary" />
-                    </a>
-                </div>
-                <div className="Card1">
-                    <a href="https://www.linkedin.com/in/shiv248/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQFaDSfdUHo1gg/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=dBvv2phu7MsK2jtILFw7PPv_GIgZcJxFFsVBaLmqw9Q" alt="Shiv" />
-                    </a>
-                </div>
-                <div className="Card1">
-                    <a href="https://www.linkedin.com/in/athena-siaotong/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQH9zr_CZIVMAQ/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=kptUKgOIPzvG3Elu-EDADdL1i0HgJJpkP9M6CPA5YBk" alt="Athena" />
-                    </a>
-                </div>
-                <div className="Card1">
-                    <a href="https://www.linkedin.com/in/adriandarian/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQGeZi-Ciku94A/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=gqZD6augIL_JI8bXt-_bx5AnihwaLINd5mJq1_ktNJM" alt="Adrian" />
-                    </a>
-                </div>
-            </div>
-
-            <div className="Container2">
-                <div className="Card2">
-                    <a href="https://www.linkedin.com/in/busher-bridi/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQHdm42_yBgoUw/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=kftsgXhyADNOcJzcWDQegbeswnhKQ2P_PPv0YOor_vs" alt="Busher" />
-                    </a>
-                </div>
-                <div className="Card2">
-                    <a href="https://www.linkedin.com/in/tuzette-vu-500550100/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQFvhoAkLHRr1Q/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=LyY4lBRCUWSO9C8Md49hQVCqAi3LnYVQFXdeYAya_Lo" alt="Tuzette" />
-                    </a>
-                </div>
-                <div className="Card2">
-                    <a href="https://www.linkedin.com/in/manjot-singh-0a41a8195/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQGnwXogrvm77A/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=YoldPpq9kb8HxZkvzwflxqH66AV2NdgUf_t1BvAai-A" alt="Manjot" />
-                    </a>
-                </div>
-                <div className="Card2">
-                    <a href="https://www.linkedin.com/in/kacey-su-351950176/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C5603AQE_tI2CyMuGOA/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=CEd--ADpJO7tdZoEB5jkGJJzpo4-tVRQUzD7diQfyMQ" alt="Kacey" />
-                    </a>
-                </div>
-                <div className="Card2">
-                    <a href="https://www.linkedin.com/in/sidharth-babu/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4D03AQG-wdMtjaAf-w/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=kAGm6x-RdyKXQkObqYiN7bKRt9TyLFh5zryFRQnWI3g" alt="Sid" />
-                    </a>
-                </div>
-                <div className="Card2">
-                    <a href="https://www.linkedin.com/in/intiseraziz/">
-                        <img className="Picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQFM85P-XiR-aQ/profile-displayphoto-shrink_200_200/0?e=1596067200&amp;v=beta&amp;t=S3Q9hdinguD7DC9aJHYbmZYb9fERKmVaQ0_Dna93bBo" alt="Intiser" />
-                    </a>
-                </div>
-            </div>
-        </div>
+            <ul id="grid" className="clear">
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/trumanjfchan/">
+                            <img className="Picture" src={TRUMAN} alt="Truman" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/lorenzo-scaturchio-62203b160/">
+                            <img className="Picture" src={LORENZO} alt="Lorenzo" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/maryfrancine/">
+                            <img className="Picture" src={MARY} alt="Mary" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/shiv248/">
+                            <img className="Picture" src={TRUMAN} alt="Shiv" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/athena-siaotong/">
+                            <img className="Picture" src={TRUMAN} alt="Athena" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/adriandarian/">
+                            <img className="Picture" src={TRUMAN} alt="Adrian" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/busher-bridi/">
+                            <img className="Picture" src={TRUMAN} alt="Busher" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/tuzette-vu-500550100/">
+                            <img className="Picture" src={TRUMAN} alt="Tuzette" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/manjot-singh-0a41a8195/">
+                            <img className="Picture" src={TRUMAN} alt="Manjot" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/kacey-su-351950176/">
+                            <img className="Picture" src={TRUMAN} alt="Kacey" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/sidharth-babu/">
+                            <img className="Picture" src={TRUMAN} alt="Sid" />
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="hexagon">
+                        <a href="https://www.linkedin.com/in/intiseraziz/">
+                            <img className="Picture" src={TRUMAN} alt="Intiser" />
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </body>
     );
 };
 
-export default TeamPics;
+export default Test;
