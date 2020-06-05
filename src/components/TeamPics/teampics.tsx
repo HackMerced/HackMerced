5297d8122a81065bb3fd1e9a095dd48e017fb626
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-06-04 22:58:03 -0700

    add: team pictures

--- src/components/TeamPics/teampics.tsx
+++ src/components/TeamPics/teampics.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
-import TRUMAN from '../../assets/images/truman.jpg';
-import LORENZO from '../../assets/images/lorenzo.jpg';
-import MARY from '../../assets/images/mary.jpg';
-//import SHIV from '../../assets/images/shiv.jpg';
-//import ATHENA from '../../assets/images/athena.jpg';
-//import ADRIAN from '../../assets/images/adrian.jpg';
-//import BUSHER from '../../assets/images/busher.jpg';
-//import TUZETTE from '../../assets/images/tuzette.jpg';
-//import MANJOT from '../../assets/images/manjot.jpg';
-//import KACEY from '../../assets/images/kacey.jpg';
-//import SID from '../../assets/images/sid.jpg';
-//import INTISER from '../../assets/images/intiser.jpg';
+import TRUMAN from '../../assets/images/truman1.jpg';
+import LORENZO from '../../assets/images/lorenzo1.jpg';
+import MARY from '../../assets/images/mary1.jpg';
+import SHIV from '../../assets/images/shiv1.jpg';
+import ATHENA from '../../assets/images/athena1.jpg';
+import ADRIAN from '../../assets/images/adrian1.jpg';
+import BUSHER from '../../assets/images/busher.jpg';
+import TUZETTE from '../../assets/images/tuzette.jpg';
+import MANJOT from '../../assets/images/manjot1.jpg';
+import KACEY from '../../assets/images/kacey.jpg';
+// import SID from '../../assets/images/sid.jpg';
+// import INTISER from '../../assets/images/intiser.jpg';
 
 import './teampics.scss';
 
-
 /* Outline for this component largely taken from: https://www.codesmite.com/article/how-to-create-pure-css-hexagonal-grids */
-
 
 const Test: React.FC = () => {
     return (
@@ -51,66 +49,66 @@ const Test: React.FC = () => {
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/shiv248/">
-                            <img className="Picture" src={TRUMAN} alt="Shiv" />
+                            <img className="Picture" src={SHIV} alt="Shiv" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/athena-siaotong/">
-                            <img className="Picture" src={TRUMAN} alt="Athena" />
+                            <img className="Picture" src={ATHENA} alt="Athena" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/adriandarian/">
-                            <img className="Picture" src={TRUMAN} alt="Adrian" />
+                            <img className="Picture" src={ADRIAN} alt="Adrian" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/busher-bridi/">
-                            <img className="Picture" src={TRUMAN} alt="Busher" />
+                            <img className="Picture" src={BUSHER} alt="Busher" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/tuzette-vu-500550100/">
-                            <img className="Picture" src={TRUMAN} alt="Tuzette" />
+                            <img className="Picture" src={TUZETTE} alt="Tuzette" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/manjot-singh-0a41a8195/">
-                            <img className="Picture" src={TRUMAN} alt="Manjot" />
+                            <img className="Picture" src={MANJOT} alt="Manjot" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/kacey-su-351950176/">
-                            <img className="Picture" src={TRUMAN} alt="Kacey" />
+                            <img className="Picture" src={KACEY} alt="Kacey" />
                         </a>
                     </div>
                 </li>
-                <li>
+                {/* <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/sidharth-babu/">
-                            <img className="Picture" src={TRUMAN} alt="Sid" />
+                            <img className="Picture" src={SID} alt="Sid" />
                         </a>
                     </div>
                 </li>
                 <li>
                     <div className="hexagon">
                         <a href="https://www.linkedin.com/in/intiseraziz/">
-                            <img className="Picture" src={TRUMAN} alt="Intiser" />
+                            <img className="Picture" src={INTISER} alt="Intiser" />
                         </a>
                     </div>
-                </li>
+                </li> */}
             </ul>
         </body>
     );
