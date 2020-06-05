9cf8d6d7016bf0e9c7cad5dd956eb6c5b0d40e70
snakethebigboss <intiseraziz@gmail.com>
2020-06-04 23:01:08 -0700

    add: about us

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Title from '../../components/Title/title';
 import Navbar from '../../components/NavBar/navbar';
-
+import About from '../../components/About Us/about';
 const Home: React.FC = () => {
     return (
         <div className="">
             <Navbar />
             <Title />
+            <About />
         </div>
     );
 };
