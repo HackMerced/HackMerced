816b8a2b215c8f9fe21b27814194efb4ab05c6be
snakethebigboss <intiseraziz@gmail.com>
2020-06-04 23:01:08 -0700

    add: about us

---
+++ src/components/About Us/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './about.scss';
+
+
+const About: React.FC = () => {
+  return (
+    <div className="About">
+      <h1>ABOUT US</h1>
+      <p>Filler paragraph text stuff about us</p>
+    </div>
+  );
+};
+
+export default About;
