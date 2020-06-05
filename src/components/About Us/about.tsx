9dc7cd952efd5a510a23311256a84078f71bb322
snakethebigboss <intiseraziz@gmail.com>
2020-06-04 23:38:01 -0700

    add: team picture

--- src/components/About Us/about.tsx
+++ src/components/About Us/about.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import TEAM from "../../assets/images/header.jpg";
 import './about.scss';
 
 
 const About: React.FC = () => {
   return (
+    <section className="ABOUT">
     <div className="About">
       <h1>ABOUT US</h1>
       <p>Filler paragraph text stuff about us</p>
     </div>
+    <div className="TEAMPICTURE">
+
+      <img src={TEAM} width="50%" alt="HackMerced Team"/>
+    </div>
+    <div className="about-us-text">
+      <p>
+        hellloo
+      </p>
+
+    </div>
+    </section>
   );
 };
 
