b41da505ccb7be00aabfcaadc5a2754ed016b567
snakethebigboss <intiseraziz@gmail.com>
2020-06-05 01:29:35 -0700

    coded the about us page

--- src/components/About Us/about.tsx
+++ src/components/About Us/about.tsx
@@ -7,18 +7,15 @@ const About: React.FC = () => {
   return (
     <section className="ABOUT">
     <div className="About">
-      <h1>ABOUT US</h1>
-      <p>Filler paragraph text stuff about us</p>
+      <h1><strong>ABOUT US</strong></h1>
+      <p>HackMerced is a 36 hour annual programming competition that occurs at the University of California, Merced and is open to students from all over the world. During the event, participants will collaborate in teams and attend workshops to learn about new technologies.
+      </p>
+      <p><br></br>We aim to create a collaborative, interdisciplinary event that brings together students from all universities and prospective sponsors to see the innovation and creativity culminating within the San Joaquin Valley.
+      </p>
     </div>
     <div className="TEAMPICTURE">
 
-      <img src={TEAM} width="50%" alt="HackMerced Team"/>
-    </div>
-    <div className="about-us-text">
-      <p>
-        hellloo
-      </p>
-
+      <img src={TEAM} width= "100%" alt="HackMerced Team"/>
     </div>
     </section>
   );
