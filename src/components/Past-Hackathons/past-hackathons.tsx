28abccc5e0aeb7239422f1ad331afe12cab87440
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-06-08 23:43:50 -0700

    add: intitial setup

---
+++ src/components/Past-Hackathons/past-hackathons.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './past-hackathons.scss';
+
+const PastHackathons: React.FC = () => {
+    return <section className="PAST-HACKATHONS"></section>;
+};
+
+export default PastHackathons;
