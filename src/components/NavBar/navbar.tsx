14aae5739f9e85c151d87ed025b6d5891f443c27
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-06-23 22:22:20 -0700

    fix: sponsor us text

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -39,12 +39,12 @@ const Navbar: React.FC = () => {
                         </a>
                     </li>
                     <li className="mr-3">
-                        <a
+                        <Link
                             className="inline-block rounded font-bold hover:text-black hover:bg-white py-2 px-3 text-gray-100 "
-                            href="http://localhost:3000/"
+                            to="sponsors"
                         >
                             SPONSOR US
-                        </a>
+                        </Link>
                     </li>
                     <li>
                         <a
