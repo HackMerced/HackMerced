f28ebf55c689d18ec158b495d97cffb15b22044c
adriandarian <adarian@ucmerced.edu>
2020-07-03 15:58:30 -0700

    cleaned up Title Component

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,22 +1,10 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import HMTITLE from '../../assets/images/placeholder-title.png';
 import HMTOWER from '../../assets/images/tower.png';
 import './title.scss';
 
-const styles2 = {
-    width: '400px',
-    height: '175px',
-};
-const styles3 = {
-    width: '525px',
-    height: '1005px',
-};
-// const styles4 = {
-//     width: '350px',
-//     height: '405px',
-// };
-const Title: React.FC = () => {
+const Title: FC = (): JSX.Element => {
     return (
         <section className="TITLE ">
             <div className="t-wave">
@@ -26,14 +14,9 @@ const Title: React.FC = () => {
                 <div className="cloud c4"></div>
                 <div className="cloud c5"></div>
             </div>
-            {/* <div className="cloud fcloud01"></div>
-            <div className="cloud fcloud02"></div> */}
             <div className="hackmerced-title">
-                <img src={HMTITLE} style={styles2} alt="HACKMERCED TITLE" />
+                <img src={HMTITLE} width="400" height="175" alt="HACKMERCED TITLE" />
             </div>
-            {/* <div className="font-black TITLE-TEXT">
-                HackMerced <br />{' '}
-            </div> */}
             <div className="title-subtext text-gray-100 ">The biggest San Joaquin Valley hackathon.</div>
             <div className="title-text text-gray-100 ">
                 Stay updated with HackMerced and subscribe to our email list!
@@ -73,11 +56,8 @@ const Title: React.FC = () => {
                 </div>
             </div>
             <div className="hackmerced-tower">
-                <img src={HMTOWER} style={styles3} alt="HACKMERCED TITLE" />
+                <img src={HMTOWER} width="525" height="1005" alt="HACKMERCED TITLE" />
             </div>
-            {/* <div className="hackmerced-statue">
-                <img src={HMSTATUE} style={styles4} alt="HACKMERCED STATUE" />
-            </div> */}
         </section>
     );
 };
