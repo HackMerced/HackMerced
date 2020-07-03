94c5f1c1d59942ff09b68e1c43265d071ae48ef3
adriandarian <adarian@ucmerced.edu>
2020-07-03 15:38:32 -0700

    fix: form to allow props and added an animation
    animation broke the design, so it is still a WIP

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 
 import './Form.scss';
+import { FormProps, FormState } from './Form.types';
 
-class Form extends Component {
+class Form extends Component<FormProps, FormState> {
     state = {
         name: '',
         email: '',
+        company: '',
         message: '',
     };
 
-    constructor(props: Readonly<{}>) {
+    constructor(props: Readonly<FormProps>) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+    handleSubmit(event: React.ChangeEvent<HTMLFormElement>): void {
         event.preventDefault();
+
         Axios({
             method: 'POST',
             url: 'http://localhost:3002/send',
@@ -31,38 +34,72 @@ class Form extends Component {
         });
     }
 
-    resetForm() {
-        this.setState({ name: '', email: '', message: '' });
+    resetForm(): void {
+        const reset = !this.props.askCompany
+            ? { name: '', email: '', message: '' }
+            : { name: '', company: '', email: '', message: '' };
+        this.setState(reset);
     }
 
     render(): JSX.Element {
         const { name, email, message } = this.state;
+        const { formTitle, askCompany } = this.props;
+
         return (
             <form className="splash-form" onSubmit={this.handleSubmit}>
-                <h3>Message Us</h3>
-                <input
-                    placeholder="Full Name"
-                    value={name}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        this.setState({ name: event.target.value });
-                    }}
-                />
-                <input
-                    placeholder="Email"
-                    aria-describedby="emailHelp"
-                    value={email}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        this.setState({ email: event.target.value });
-                    }}
-                />
-                <textarea
-                    placeholder="Message"
-                    rows={5}
-                    value={message}
-                    onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        this.setState({ message: event.target.value });
-                    }}
-                />
+                <h3>{formTitle}</h3>
+                <div className="input-wrapper">
+                    <input
+                        id="name"
+                        required
+                        placeholder="Full Name"
+                        value={name}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            this.setState({ name: event.target.value });
+                        }}
+                    />
+                    <label htmlFor="name">user name</label>
+                </div>
+                {askCompany ? (
+                    <div className="input-wrapper">
+                        <input
+                            id="company"
+                            required
+                            placeholder="Company Name"
+                            value={name}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                this.setState({ name: event.target.value });
+                            }}
+                        />
+                        <label htmlFor="company">user name</label>
+                    </div>
+                ) : null}
+                <div className="input-wrapper">
+                    <input
+                        id="email"
+                        required
+                        placeholder="Email"
+                        aria-describedby="emailHelp"
+                        value={email}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            this.setState({ email: event.target.value });
+                        }}
+                    />
+                    <label htmlFor="email">user name</label>
+                </div>
+                <div className="input-wrapper">
+                    <textarea
+                        id="message"
+                        required
+                        placeholder="Message"
+                        rows={5}
+                        value={message}
+                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                            this.setState({ message: event.target.value });
+                        }}
+                    />
+                    <label htmlFor="message">user name</label>
+                </div>
                 <button type="submit">Send</button>
             </form>
         );
