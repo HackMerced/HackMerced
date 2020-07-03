136c0136bea6f955019add380a81160ea428c4b1
adriandarian <adarian@ucmerced.edu>
2020-07-03 15:38:47 -0700

    fix: contact us page

--- src/pages/Contact-Us/contact-us.tsx
+++ src/pages/Contact-Us/contact-us.tsx
@@ -1,14 +1,27 @@
 import React, { FC } from 'react';
 
 import NavbarOrange from '../../components/NavBar-Orange/navbar';
-import Contact from '../../components/Contact-Form/contact-form';
+import Form from '../../components/Form/Form';
 import Footer from '../../components/Footer/footer';
 
 const ContactUs: FC = (): JSX.Element => {
     return (
         <>
             <NavbarOrange />
-            <Contact />
+            <main className="contact-body">
+                <div className="splash-content">
+                    <section className="Header">
+                        <h1>Ways to contact us</h1>
+                    </section>
+                    <section className="splash-section">
+                        <h4>Email us at general@hackmerced.com</h4>
+                    </section>
+                    <section className="splash-section">
+                        <h4>Connect with our social medias: @hackmerced</h4>
+                    </section>
+                    <Form formTitle="Message Us" askCompany={false} />
+                </div>
+            </main>
             <Footer />
         </>
     );
