eaeb0f1f26e30741cc7b82bb753c4923acea4da2
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-07-03 21:12:13 -0700

    add: more animated content

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -33,6 +33,32 @@ const Footer: FC = () => {
     return (
         <footer className="footer">
             <div className="gooey-animations"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="15" result="blur" />
+                        <feColorMatrix
+                            in="blur"
+                            mode="matrix"
+                            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 15 -7"
+                            result="goo"
+                        />
+                        <feBlend in="SourceGraphic" in2="goo" />
+                    </filter>
+                </defs>
+            </svg>
+
+            <svg viewbox="0 0 1440 328" width="100vw">
+                <defs>
+                    <clipPath
+                        id="wave"
+                        clipPathUnits="objectBoundingBox"
+                        transform="scale(0.00069444444, 0.00304878048)"
+                    >
+                        <path d="M504.452 27.7002C163.193 -42.9551 25.9595 38.071 0 87.4161V328H1440V27.7002C1270.34 57.14 845.711 98.3556 504.452 27.7002Z" />
+                    </clipPath>
+                </defs>
+            </svg>
             <section className="Footer-container">
                 <nav className="Footer-social">
                     <a
