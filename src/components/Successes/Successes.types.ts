193dd9a585b510084a9af86a83d31fd2ed4be4c5
adriandarian <adarian@ucmerced.edu>
2020-07-03 18:20:31 -0700

    fix: replaced all the success components with a successes component

---
+++ src/components/Successes/Successes.types.ts
@@ -0,0 +1,9 @@
+export type SuccessesProps = {
+  hoursOfHacking: number;
+  numberOfAttendees: number;
+  projectsSubmitted: number;
+  numberOfPrizes: number;
+  numberOfWorkshops: number;
+  numberOfSponsors: number;
+  iteration: string | null;
+}
