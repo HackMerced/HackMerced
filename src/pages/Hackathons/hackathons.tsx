ae47f3559e388a536dcbbc406ffd39265661cca9
adriandarian <adarian@ucmerced.edu>
2020-07-03 17:51:48 -0700

    fix: made a single PastHackathonsGallery component instead of multiple

--- src/pages/Hackathons/hackathons.tsx
+++ src/pages/Hackathons/hackathons.tsx
@@ -1,34 +1,70 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-import Navbar1 from '../../components/NavBar/navbar';
+import Navbar from '../../components/NavBar/navbar';
 import SuccessV from '../../components/Success-V/success-v';
-import HackMercedV from '../../components/HackMerced-V/hackmerced-v';
 import SuccessIV from '../../components/Success-IV/success-iv';
-import HackMercedIV from '../../components/HackMerced-IV/hackmerced-iv';
 import SuccessIII from '../../components/Success-III/success-iii';
-import HackMercedIII from '../../components/HackMerced-III/hackmerced-iii';
 import SuccessII from '../../components/Success-II/success-ii';
-import HackMercedII from '../../components/HackMerced-II/hackmerced-ii';
 import Success from '../../components/Success/success';
-import HackMerced from '../../components/HackMerced/hackmerced';
 import Footer from '../../components/Footer/footer';
+import PastHackathonsGallery from '../../components/PastHackathonsGallery/PastHackathonsGallery';
 
-const Home: React.FC = () => {
+// HackMerced I
+import CHECKIN from '../../assets/images/checkin.jpg';
+import HMFOOD from '../../assets/images/hmfood.jpg';
+import MACHINE from '../../assets/images/machine.jpg';
+import TEACHING from '../../assets/images/teaching.jpg';
+import OLDTEAM from '../../assets/images/team-2016.jpg';
+import TUTOR from '../../assets/images/tutor.jpg';
+
+// HackMerced II
+import SPEAKERII from '../../assets/images/speaker-ii.jpg';
+import HACKERSII from '../../assets/images/hackers-ii.jpg';
+import COLLABORATION from '../../assets/images/collaboration.jpg';
+import CHECKINGINII from '../../assets/images/checking-in-ii.jpg';
+import MLHII from '../../assets/images/mlh-ii.jpg';
+import SPONSORSII from '../../assets/images/sponsors-ii.jpg';
+
+// HackMerced III
+import MLHCUPSTACK from '../../assets/images/mlh-cupstack.jpg';
+import WINNERSIII from '../../assets/images/winners-iii.jpg';
+import AWARDSIII from '../../assets/images/awards-iii.jpg';
+import PRIZES from '../../assets/images/prizes-iii.jpg';
+import JUDGESIII from '../../assets/images/judges-iii.jpg';
+import REDBULL from '../../assets/images/redbull.jpg';
+
+// HackMerced IV
+import HACKING from '../../assets/images/hackers2.jpg';
+import PRESENTER from '../../assets/images/presenter.jpg';
+import PRESENTATIONS from '../../assets/images/presentations.jpg';
+import HACKIV from '../../assets/images/hackiv.jpg';
+import JUDGES from '../../assets/images/judges.jpg';
+import AWARDS from '../../assets/images/awards2.jpg';
+
+// HackMerced V
+import HACKERS from '../../assets/images/hackers.jpg';
+import HMFOODV from '../../assets/images/hmfood-v.jpg';
+import CITRIS from '../../assets/images/citris.jpg';
+import SPONSORSV from '../../assets/images/sponsors-v.jpg';
+import HEALTHWINNER from '../../assets/images/health2.jpg';
+import SHUBAWARD from '../../assets/images/shub-award.jpg';
+
+const Home: FC = (): JSX.Element => {
     return (
-        <div className="">
-            <Navbar1 />
+        <main>
+            <Navbar />
             <SuccessV />
-            <HackMercedV />
+            <PastHackathonsGallery iteration={"V"} images={[HACKERS, HMFOODV, CITRIS, SPONSORSV, HEALTHWINNER, SHUBAWARD]} />
             <SuccessIV />
-            <HackMercedIV />
+            <PastHackathonsGallery iteration={"IV"} images={[HACKING, PRESENTER, PRESENTATIONS, HACKIV, JUDGES, AWARDS]} />
             <SuccessIII />
-            <HackMercedIII />
+            <PastHackathonsGallery iteration={"III"} images={[MLHCUPSTACK, WINNERSIII, AWARDSIII, PRIZES, JUDGESIII, REDBULL]} />
             <SuccessII />
-            <HackMercedII />
+            <PastHackathonsGallery iteration={"I"} images={[SPEAKERII, HACKERSII, COLLABORATION, CHECKINGINII, MLHII, SPONSORSII]} />
             <Success />
-            <HackMerced />
+            <PastHackathonsGallery iteration={null} images={[CHECKIN, HMFOOD, MACHINE, TEACHING, OLDTEAM, TUTOR]} />
             <Footer />
-        </div>
+        </main>
     );
 };
 
