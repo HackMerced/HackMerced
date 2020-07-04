31b6b327a118d247e140643a9b67de89f7ef7b72
adriandarian <adarian@ucmerced.edu>
2020-07-03 17:07:53 -0700

    lint: test files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,25 @@
-import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
-import App from "./App";
+import App from './App';
 
 let container: HTMLDivElement;
 beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+    // setup a DOM element as a render target
+    container = document.createElement('div');
+    document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
 });
 
-it("renders home page", () => {
-  act(() => {
-    render(<App />, container);
-  });
-  expect(container.textContent).toContain("HackMerced");
+it('renders home page', () => {
+    act(() => {
+        render(<App />, container);
+    });
+    expect(container.textContent).toContain('HackMerced');
 });
