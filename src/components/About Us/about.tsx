593b119673fb3408203877a0db03ad4269e83dc9
adriandarian <adarian@ucmerced.edu>
2020-07-10 18:10:04 -0700

    fix: semantic HTML in about us section

--- src/components/About Us/about.tsx
+++ src/components/About Us/about.tsx
@@ -6,22 +6,17 @@ import './about.scss';
 const About: FC = (): JSX.Element => {
     return (
         <section className="ABOUT">
-            <div className="About">
+            <article className="About">
                 <h1>About Us</h1>
                 <p>
-                    HackMerced is a 36 hour annual programming competition that occurs at the University of California,
-                    Merced and is open to students from all over the world. During the event, participants will
-                    collaborate in teams and attend workshops to learn about new technologies.
+                    HackMerced is a 36 hour annual programming competition that occurs at the University of California, Merced and is open to students from all over the world. During the event, participants will collaborate in teams and attend workshops to learn about new technologies.
+                    <br />
+                    We aim to create a collaborative, interdisciplinary event that brings together students from all universities and prospective sponsors to see the innovation and creativity culminating within the San Joaquin Valley.
                 </p>
-                <p>
-                    <br />We aim to create a collaborative, interdisciplinary event that brings together students
-                    from all universities and prospective sponsors to see the innovation and creativity culminating
-                    within the San Joaquin Valley.
-                </p>
-            </div>
-            <div className="TEAMPICTURE">
+            </article>
+            <aside className="TEAMPICTURE">
                 <img src={TEAM} width="100%" alt="HackMerced Team" />
-            </div>
+            </aside>
         </section>
     );
 };
