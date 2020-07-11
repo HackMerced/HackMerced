231862cc1bbba241d0dedf37604e723a11944326
adriandarian <adarian@ucmerced.edu>
2020-07-10 18:03:54 -0700

    fix: mobile view for title section

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,64 +1,57 @@
 import React, { FC } from 'react';
 
+import useWindowDimensions from '../WindowDimensions/WindowDimensions';
 import HMTITLE from '../../assets/images/placeholder-title.png';
 import HMTOWER from '../../assets/images/tower.png';
 import './title.scss';
 
 const Title: FC = (): JSX.Element => {
+    const { width } = useWindowDimensions();
+
     return (
-        <section className="TITLE ">
-            <div className="t-wave">
+        <header className="TITLE">
+            <section className="t-wave">
                 <div className="cloud c1"></div>
                 <div className="cloud c2"></div>
                 <div className="cloud c3"></div>
                 <div className="cloud c4"></div>
                 <div className="cloud c5"></div>
-            </div>
-            <div className="hackmerced-title">
-                <img src={HMTITLE} width="400" height="175" alt="HACKMERCED TITLE" />
-            </div>
-            <div className="title-subtext text-gray-100 ">The biggest San Joaquin Valley hackathon.</div>
-            <div className="title-text text-gray-100 ">
-                Stay updated with HackMerced and subscribe to our email list!
-            </div>
-            <button
-                onClick={(): void => {
-                    window.location.href = '#popup3';
-                }}
-                className="bg-white text-black BUTTON font-bold"
-            >
-                Subscribe
-            </button>
-
-            <div id="popup3" className="overlay light">
-                <a className="cancel" href="#"></a>
-                <div className="popup1">
-                    <h3>HackMerced Mailing List</h3>
-                    <div className="formpopup1">
-                        <form>
-                            <div className="labels-form">
-                                <h2>Name</h2>
-                            </div>
-                            <input placeholder="Name" type="text" required></input>
-                            <br></br>
-                            <div className="labels-form">
-                                <h2>Email</h2>
-                            </div>
-                            <input placeholder="Email" type="email" required></input>
-                            <br></br>
-                            <div className="formbutton">
-                                <button className="button4">Submit</button>
-                            </div>
-                        </form>
-                    </div>
-                    <br></br>
-                    <br></br>
+            </section>
+            <section className="hackmerced">
+                <img src={HMTITLE} width="400" height="175" alt="HACKMERCED TITLE" className="hackmerced-title" />
+                <div className="hackmerced-subtext text-gray-100">The biggest San Joaquin Valley hackathon.</div>
+                <div className="hackmerced-text text-gray-100">
+                    Stay updated with HackMerced and subscribe to our email list!
                 </div>
+                <button
+                    className="bg-white text-black font-bold"
+                    onClick={(): void => {
+                        window.location.href = '#subscribe-modal';
+                    }}
+                >
+                    Subscribe
+                </button>
+            </section>
+            <div id="subscribe-modal" className="overlay light">
+                <a className="cancel" href="#"></a>
+                <article className="subscribe">
+                    <h3>HackMerced Mailing List</h3>
+                    <form className="form">
+                        <h2 className="form-label">Name</h2>
+                        <input placeholder="Name" type="text" required />
+                        <br />
+                        <h2 className="form-label">Email</h2>
+                        <input placeholder="Email" type="email" required />
+                        <br />
+                        <button className="form-submit-button">Submit</button>
+                    </form>
+                    <br />
+                </article>
             </div>
-            <div className="hackmerced-tower">
-                <img src={HMTOWER} width="525" height="1005" alt="HACKMERCED TITLE" />
-            </div>
-        </section>
+            {width > 500 ? (
+                <img src={HMTOWER} width="525" height="1005" alt="HACKMERCED TITLE" className="hackmerced-tower" />
+            ) : null}
+        </header>
     );
 };
 
