8b4e246d8fa0220e582637648babfeab1cb9acf9
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-07-10 17:53:05 -0700

    add: icons to contact-us

--- src/pages/Contact-Us/contact-us.tsx
+++ src/pages/Contact-Us/contact-us.tsx
@@ -3,6 +3,8 @@ import React, { FC } from 'react';
 import NavbarOrange from '../../components/NavBar-Orange/navbar';
 import Form from '../../components/Form/Form';
 import Footer from '../../components/Footer/footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 import './contact-us.scss';
 
@@ -16,9 +18,15 @@ const ContactUs: FC = (): JSX.Element => {
                         <h1>Ways to contact us</h1>
                     </section>
                     <section className="splash-section">
+                        <a style={{ color: '#0000' }}>
+                            <FontAwesomeIcon icon={faEnvelope} className="fa" />
+                        </a>
                         <h4>Email us at general@hackmerced.com</h4>
                     </section>
                     <section className="splash-section">
+                        <a style={{ color: '#0000' }}>
+                            <FontAwesomeIcon icon={faUsers} className="fa" />
+                        </a>
                         <h4>Connect with our social medias: @hackmerced</h4>
                     </section>
                     <Form formTitle="Message Us" askCompany={false} />
