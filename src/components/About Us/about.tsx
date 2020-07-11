9104de1a0d2eb1dfe0ab9b0aec951b97dd0e9a80
adriandarian <adarian@ucmerced.edu>
2020-07-10 20:01:52 -0700

    fix: responsive about us section

--- src/components/About Us/about.tsx
+++ src/components/About Us/about.tsx
@@ -5,18 +5,21 @@ import './about.scss';
 
 const About: FC = (): JSX.Element => {
     return (
-        <section className="ABOUT">
-            <article className="About">
+        <section className="about">
+            <article className="about-description">
                 <h1>About Us</h1>
                 <p>
-                    HackMerced is a 36 hour annual programming competition that occurs at the University of California, Merced and is open to students from all over the world. During the event, participants will collaborate in teams and attend workshops to learn about new technologies.
+                    HackMerced is a 36 hour annual programming competition that occurs at the University of California,
+                    Merced and is open to students from all over the world. During the event, participants will
+                    collaborate in teams and attend workshops to learn about new technologies.
                     <br />
-                    We aim to create a collaborative, interdisciplinary event that brings together students from all universities and prospective sponsors to see the innovation and creativity culminating within the San Joaquin Valley.
+                    <br />
+                    We aim to create a collaborative, interdisciplinary event that brings together students from all
+                    universities and prospective sponsors to see the innovation and creativity culminating within the
+                    San Joaquin Valley.
                 </p>
             </article>
-            <aside className="TEAMPICTURE">
-                <img src={TEAM} width="100%" alt="HackMerced Team" />
-            </aside>
+            <img src={TEAM} width="100%" alt="HackMerced Team" className="TEAMPICTURE" />
         </section>
     );
 };
