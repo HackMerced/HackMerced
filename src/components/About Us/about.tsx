6c603bdfa2ab88f120836ed5bbe536cff9aace45
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-07-12 18:37:27 -0700

    fix: responsiveness about us

--- src/components/About Us/about.tsx
+++ src/components/About Us/about.tsx
@@ -5,21 +5,23 @@ import './about.scss';
 
 const About: FC = (): JSX.Element => {
     return (
-        <section className="about">
-            <article className="about-description">
-                <h1>About Us</h1>
-                <p>
-                    HackMerced is a 36 hour annual programming competition that occurs at the University of California,
-                    Merced and is open to students from all over the world. During the event, participants will
-                    collaborate in teams and attend workshops to learn about new technologies.
-                    <br />
-                    <br />
-                    We aim to create a collaborative, interdisciplinary event that brings together students from all
-                    universities and prospective sponsors to see the innovation and creativity culminating within the
-                    San Joaquin Valley.
-                </p>
-            </article>
-            <img src={TEAM} width="100%" alt="HackMerced Team" className="TEAMPICTURE" />
+        <section className="ABOUT">
+            <h1>About Us</h1>
+            <section className="about">
+                <article className="about-description">
+                    <p>
+                        HackMerced is a 36 hour annual programming competition that occurs at the University of
+                        California, Merced and is open to students from all over the world. During the event,
+                        participants will collaborate in teams and attend workshops to learn about new technologies.
+                        <br />
+                        <br />
+                        We aim to create a collaborative, interdisciplinary event that brings together students from all
+                        universities and prospective sponsors to see the innovation and creativity culminating within
+                        the San Joaquin Valley.
+                    </p>
+                </article>
+                <img src={TEAM} width="100%" alt="HackMerced Team" className="TEAMPICTURE" />
+            </section>
         </section>
     );
 };
