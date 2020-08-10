a0f7ec699cae1fbe665709b9e5a27612e227dc95
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-08-09 22:10:00 -0700

    revert: new footer to old

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faCode } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faInstagram, faTwitter, faSlack, faGithub } from '@fortawesome/free-brands-svg-icons';
-//import { Link } from 'react-router-dom';
+import { faFacebook, faInstagram, faGithub, faSlack } from '@fortawesome/free-brands-svg-icons';
 
 import './footer.scss';
 
@@ -10,25 +9,15 @@ const Footer: FC = (): JSX.Element => {
     return (
         <footer className="footer">
             <section className="Footer-container">
-                <section className="HackMerced">
-                    <h1>
-                        HackMerced
-                    </h1>
-                </section>
-
                 <nav className="Footer-social">
-                    <a href="https://twitter.com/hackmerced/">
-                        <FontAwesomeIcon icon={faTwitter} className="fa text-white" />
-                    </a>
-                    <a href="https://www.instagram.com/hackmerced/" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faInstagram} className="fa text-white" />
+                    <a href="mailto:general@hackmerced.com?subject=HackMerced Questions&amp;body=Dear HackMerced,">
+                        <FontAwesomeIcon icon={faEnvelope} className="fa text-white" />
                     </a>
                     <a href="https://www.facebook.com/hackmerced/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faFacebook} className="fa text-white" />
                     </a>
-
-                    <a href="mailto:general@hackmerced.com?subject=HackMerced Questions&amp;body=Dear HackMerced,">
-                        <FontAwesomeIcon icon={faEnvelope} className="fa text-white" />
+                    <a href="https://www.instagram.com/hackmerced/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} className="fa text-white" />
                     </a>
                     <a
                         href="https://join.slack.com/t/hackmerced-v/shared_invite/enQtODUyNzkxMDY2NjkyLTZiODAyZjNiYTFjZTlkYTQ0NDVhOGQ5OTI3OTEzNjAxMDBiZTdjY2QyMTI0ZGE4MTlhMmNhMGYyNzQzOGNhZTI"
@@ -48,18 +37,7 @@ const Footer: FC = (): JSX.Element => {
                         <FontAwesomeIcon icon={faCode} className="fa text-white" />
                     </a>
                 </nav>
-
-                <nav className="Footer-navbar">
-                    <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/past-hackathons">Past Hackathons</a></li>
-                    <li><a href="/sponsors">Sponsors</a></li>
-                    <li><a href="/contact-us">Contact Us</a></li>
-                    </ul>
-                </nav>
-                     
-                   
-            </section> 
+            </section>
             <section className="credits">
                 <span className="copyright">
                     © 2020 HackMerced. All Rights Reserved.
