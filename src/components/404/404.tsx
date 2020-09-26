476cf888f128a26e21006eeeaffcdbe689bd95d0
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-09-25 21:25:19 -0700

    add: 404 drafted page

---
+++ src/components/404/404.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import './404.scss';
+
+const Error: FC = (): JSX.Element => {
+    return (
+        <>
+            <main>
+                <div className="error"></div>
+            </main>
+        </>
+    );
+};
+
+export default Error;
