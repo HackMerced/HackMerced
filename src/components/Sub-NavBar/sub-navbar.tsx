55892a87fec3609b2e8813cba8a53f5647168543
AlissonRoss <alirosssalazar@gmail.com>
2020-09-25 21:00:16 -0700

    feat:dashboard and sub-navbar
    Set-up for the SubNavBar component and the Dashboard page. Both contain .scss and .tsx files respectively.

---
+++ src/components/Sub-NavBar/sub-navbar.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import './sub-navbar.scss';
+
+const SubNavbar: FC = () => {
+
+    return (
+        <main>
+
+        </main>
+    );
+}
+
+
+export default SubNavbar;
---
+++ src/pages/Dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import './dashboard.scss'
+
+const DashBoard: FC = (): JSX.Element => {
+    return (
+        <main></main>
+    )
+
+};
+
+export default DashBoard;
