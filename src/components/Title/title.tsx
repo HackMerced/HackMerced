bd78ad664aa9b87e59d1910718c7e3b91799ddf7
adriandarian <adarian@ucmerced.edu>
2020-09-30 02:51:48 -0700

    fix: subscribe modal and removed clouds on mobile
    Known issue all CSS went out the door when I fixed the modal

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,13 +1,26 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Axios from 'axios';
+import Modal from 'react-modal';
 
 import useWindowDimensions from '../WindowDimensions/WindowDimensions';
 import HMTITLE from '../../assets/images/placeholder-title.png';
 import HMTOWER from '../../assets/images/tower.png';
 import './title.scss';
 
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
 const Title: FC = (): JSX.Element => {
-    const [values, setValues] = useState({ name: '', email: '' });
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [values, setValues] = useState({ name: '', email_address: '' });
     const { width } = useWindowDimensions();
 
     const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
@@ -15,51 +28,62 @@ const Title: FC = (): JSX.Element => {
 
         Axios({
             method: 'POST',
-            url: 'https://hackmerced-myriagon.herokuapp.com/v1/mongo/storage',
+            url: 'https://hackmerced-myriagon.herokuapp.com/v1/mailchimp/subscribe',
             data: values,
         }).then(response => {
             console.log(response);
         });
     };
 
+    useEffect(() => {
+        Modal.setAppElement('body');
+    }, []);
+
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
         setValues({ ...values, [name]: value });
     };
 
+    const openModal = (): void => {
+        setIsOpen(true);
+    };
+
+    const closeModal = (): void => {
+        setIsOpen(false);
+    };
+
     return (
         <header className="TITLE">
-            <section className="t-wave">
-                <div className="cloud c1"></div>
-                <div className="cloud c2"></div>
-                <div className="cloud c3"></div>
-                <div className="cloud c4"></div>
-                <div className="cloud c5"></div>
-            </section>
+            {width > 700 ? (
+                <section className="t-wave">
+                    <div className="cloud c1"></div>
+                    <div className="cloud c2"></div>
+                    <div className="cloud c3"></div>
+                    <div className="cloud c4"></div>
+                    <div className="cloud c5"></div>
+                </section>
+            ) : null}
             <section className="hackmerced">
                 <img src={HMTITLE} width="400" height="175" alt="HACKMERCED TITLE" className="hackmerced-title" />
                 <div className="hackmerced-subtext text-gray-100">The biggest San Joaquin Valley hackathon.</div>
                 <div className="hackmerced-text text-gray-100">
                     Stay updated with HackMerced and subscribe to our email list!
                 </div>
-                <button
-                    className="bg-white text-black font-bold"
-                    onClick={(): void => {
-                        window.location.href = '#subscribe-modal';
-                    }}
-                >
+                <button className="bg-white text-black font-bold" onClick={openModal}>
                     Subscribe
                 </button>
-                {/* <div className="subscribe-container">
-                    <form className="subscribe">
-                        <input className="email-input" type="email" placeholder="Enter email for updates" />
-                        <button type="submit">→</button>
-                    </form>
-                </div> */}
             </section>
-            <div id="subscribe-modal" className="overlay light">
-                {/* eslint-disable-next-line */}
-                <a className="cancel" href="/#"></a>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Subscribe Modal"
+                // className="overlay light"
+                // id="subscribe-modal"
+            >
+                <button className="cancel" onClick={closeModal}>
+                    close
+                </button>
                 <article className="subscribe">
                     <h3>HackMerced Mailing List</h3>
                     <form className="form" onSubmit={handleSubmit}>
@@ -67,13 +91,19 @@ const Title: FC = (): JSX.Element => {
                         <input name="name" placeholder="Name" type="text" required onChange={handleInputChange} />
                         <br />
                         <h2 className="form-label">Email</h2>
-                        <input name="email" placeholder="Email" type="email" required onChange={handleInputChange} />
+                        <input
+                            name="email_address"
+                            placeholder="Email"
+                            type="email"
+                            required
+                            onChange={handleInputChange}
+                        />
                         <br />
                         <button className="form-submit-button">Submit</button>
                     </form>
                     <br />
                 </article>
-            </div>
+            </Modal>
             {width > 700 ? (
                 <img src={HMTOWER} width="525" height="1005" alt="HACKMERCED TITLE" className="hackmerced-tower" />
             ) : null}
