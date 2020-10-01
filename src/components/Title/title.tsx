62992a68d6ec466fd44f8167f3328030e29ac6a2
Adrian Darian <adarian@ucmerced.edu>
2020-09-30 17:53:49 -0700

    got modal working , but css needs work
    Trying to change to css grid

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,26 +1,14 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import Axios from 'axios';
-import Modal from 'react-modal';
 
 import useWindowDimensions from '../WindowDimensions/WindowDimensions';
 import HMTITLE from '../../assets/images/placeholder-title.png';
 import HMTOWER from '../../assets/images/tower.png';
 import './title.scss';
 
-const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-    },
-};
-
 const Title: FC = (): JSX.Element => {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [values, setValues] = useState({ name: '', email_address: '' });
+    const [showModal, setShowModal] = useState(false);
+    const [values, setValues] = useState({ FNAME: '', LNAME: '', email_address: '' });
     const { width } = useWindowDimensions();
 
     const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
@@ -29,15 +17,17 @@ const Title: FC = (): JSX.Element => {
         Axios({
             method: 'POST',
             url: 'https://hackmerced-myriagon.herokuapp.com/v1/mailchimp/subscribe',
-            data: values,
+            data: {
+                email_address: values.email_address,
+                merge_fields: {
+                    FNAME: values.FNAME,
+                    LNAME: values.LNAME,
+                },
+            },
         }).then(response => {
             console.log(response);
         });
     };
-
-    useEffect(() => {
-        Modal.setAppElement('body');
-    }, []);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
@@ -45,11 +35,13 @@ const Title: FC = (): JSX.Element => {
     };
 
     const openModal = (): void => {
-        setIsOpen(true);
+        setShowModal(true);
+        document.body.style.overflow = 'hidden';
     };
 
     const closeModal = (): void => {
-        setIsOpen(false);
+        setShowModal(false);
+        document.body.style.overflow = 'scroll';
     };
 
     return (
@@ -69,41 +61,63 @@ const Title: FC = (): JSX.Element => {
                 <div className="hackmerced-text text-gray-100">
                     Stay updated with HackMerced and subscribe to our email list!
                 </div>
-                <button className="bg-white text-black font-bold" onClick={openModal}>
+                <button
+                    className="bg-white text-black font-bold"
+                    // className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    onClick={openModal}
+                >
                     Subscribe
                 </button>
             </section>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Subscribe Modal"
-                // className="overlay light"
-                // id="subscribe-modal"
-            >
-                <button className="cancel" onClick={closeModal}>
-                    close
-                </button>
-                <article className="subscribe">
-                    <h3>HackMerced Mailing List</h3>
-                    <form className="form" onSubmit={handleSubmit}>
-                        <h2 className="form-label">Name</h2>
-                        <input name="name" placeholder="Name" type="text" required onChange={handleInputChange} />
-                        <br />
-                        <h2 className="form-label">Email</h2>
-                        <input
-                            name="email_address"
-                            placeholder="Email"
-                            type="email"
-                            required
-                            onChange={handleInputChange}
-                        />
-                        <br />
-                        <button className="form-submit-button">Submit</button>
-                    </form>
-                    <br />
-                </article>
-            </Modal>
+            {showModal ? (
+                <div className="overlay light" onClick={closeModal}>
+                    <article className="subscribe">
+                        <h3>HackMerced Mailing List</h3>
+                        <form className="form" onSubmit={handleSubmit}>
+                          <div className="spacer"></div>
+                            <label className="form-label first-name-label" htmlFor="firstName">
+                                First Name
+                            </label>
+                            <label className="form-label last-name-label" htmlFor="lastName">
+                                Last Name
+                            </label>
+                            <input
+                                id="firstName"
+                                className="first-name-input"
+                                name="FNAME"
+                                placeholder="First Name"
+                                type="text"
+                                required
+                                onChange={handleInputChange}
+                            />
+                            &nbsp;&nbsp;
+                            <input
+                                id="lastName"
+                                className="last-name-input"
+                                name="LNAME"
+                                placeholder="Last Name"
+                                type="text"
+                                required
+                                onChange={handleInputChange}
+                            />
+                            <br />
+                            <label className="form-label email-label" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                className="email-input"
+                                name="email_address"
+                                placeholder="Email"
+                                type="email"
+                                required
+                                onChange={handleInputChange}
+                            />
+                            <button className="form-submit-button">Submit</button>
+                        </form>
+                    </article>
+                </div>
+            ) : null}
             {width > 700 ? (
                 <img src={HMTOWER} width="525" height="1005" alt="HACKMERCED TITLE" className="hackmerced-tower" />
             ) : null}
