d25754cabf258e377b90451644edd77b4b8a5f8d
Truman Chan <trumanjfchan@gmail.com>
2020-10-01 18:41:47 -0700

    Created a Sponsors Component for Designathon
    The placeholders that are there currently show that if we test responsiveness by changing the window resolution, the placeholder logos would be hard to read. Thus an easy fix would be to upload sponsor logos that take up the entire width and height of the picture, so that each grid can be filled completely. Item1 needs to be replaced with the title asset.

--- src/pages/DesignMerced/designmerced.tsx
+++ src/pages/DesignMerced/designmerced.tsx
@@ -3,12 +3,14 @@ import React, { FC } from 'react';
 import Navbar from '../../components/NavBar/navbar';
 import Footer from '../../components/Footer/footer';
 import Schedule from '../../components/Schedule Design/schedule-design';
+import Sponsors from '../..//components/Sponsors Design/sponsors-design';
 
 const DesignMerced: FC = (): JSX.Element => {
     return (
         <main className="DESIGNMERCED">
             <Navbar backgroundColor="#EEEBF5" textColor="#B486CE" breakLineColor="#C4BDDC" showBanner={false} />
             <Schedule />
+            <Sponsors />
             <Footer backgroundColor="#EEEBF5" textColor="#B486CE" fontColor="#B486CE" />
         </main>
     );
