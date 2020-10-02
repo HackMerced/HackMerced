47bcee4ecca9eb314a15b94bf4a581a7d0d3211e
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-02 12:29:28 -0700

    fix: finish/resize sponsor design

--- src/components/Sponsors Design/sponsors-design.tsx
+++ src/components/Sponsors Design/sponsors-design.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-
+import SponsorDesign from '../../assets/images/sponsor-design1.jpg';
 import './sponsors-design.scss';
 
 const SponsorsDesign: FC = (): JSX.Element => {
@@ -8,7 +8,9 @@ const SponsorsDesign: FC = (): JSX.Element => {
             <section className="sponsor-designathon-title">
                 <div className="title5">Our Sponsors</div>
             </section>
-            <aside className="sponsor-logos"></aside>
+            <aside className="sponsor-logos">
+                <img src={SponsorDesign} width="100%" alt="Sponsors for Designathon" />
+            </aside>
         </main>
     );
 };
