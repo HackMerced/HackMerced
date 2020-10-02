30d7ac12e523459e0bbc5a76a09851bb06f3d09e
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-02 13:30:25 -0700

    fix: about us design

--- src/components/About Us Design/aboutus-design.tsx
+++ src/components/About Us Design/aboutus-design.tsx
@@ -6,24 +6,25 @@ import './aboutus-design.scss';
 const AboutUsDesign: FC = (): JSX.Element => {
     return (
         <section className="ABOUTUSDESIGN">
-            <h1>The UI/UX Prompt</h1>
+            <h1>About DesignMerced</h1>
             <section className="about">
                 <article className="about-description">
                     <div className="aboutUsText">
                         <p>
-                            DesignMerced is Merced’s first virtual hackathon for creatives and design individuals in
-                            high school and college from around the world. DesignMerced is a 24 hours event that will
-                            enhance hackers innovative skills and showcase their own uniqueness of solving art with
-                            technology.
-                            ‍
-                            Students with all levels of coding experience are encouraged to join us and discover their
-                            potential as problem-solvers, innovators, and future tech leaders. Look forward to a day of
-                            workshops,
-                            mentorship, networking, games, and more!
-                    </p>
+                            DesignMerced is HackMerced’s first virtual designathon for creatives and design individuals
+                            in high school and college from around the world. DesignMerced is a 24-hour event that will
+                            enhance designers/creators innovative skills and showcase their own uniqueness of solving
+                            art with technology. ‍<br></br>
+                            <br></br>
+                            Students with all levels of design experience are encouraged to join us and discover their
+                            potential as problem-solvers, innovators, and future designers. Look forward to a day of
+                            workshops, mentorship, networking, games, and more!
+                        </p>
                     </div>
                 </article>
-                <img src={TEAM} width="100%" alt="HackMerced Team" className="LOGO" />
+                <aside>
+                    <img src={TEAM} width="100%" alt="HackMerced Team" className="LOGO" />
+                </aside>
             </section>
         </section>
     );
