a5895c66a2079d19bbb687d8e68f6e15c662e106
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-03 01:16:04 -0700

    fix: add lightbulb image

--- src/components/DesignPrompt/designprompt.tsx
+++ src/components/DesignPrompt/designprompt.tsx
@@ -1,21 +1,27 @@
 import React, { FC } from 'react';
 
 import './designprompt.scss';
-import lightbulb from '../../assets/images/lightbulb.png';
+// import lightbulb from '../../assets/images/lightbulb.png';
+import lightbulb2 from '../../assets/images/lightbulb-layers.png';
 
 const DesignPrompt: FC = (): JSX.Element => {
     return (
         <main className="DESIGNPROMPT">
-            <article>
+            <div className="ui-ux-title">The UI/UX Prompt</div>
+            <img className="lightbulb2" src={lightbulb2} alt="Lightbulb Icon"></img>
+
+            {/* <article>
+                <div className="ui-ux-title">The UI/UX Prompt</div>
                 <section className="PromptLeft">
                     <div id="one" className="circle"></div>
                     <div id="two" className="circle"></div>
                 </section>
-                <img src={lightbulb} width="80%" alt="Lightbulb Icon" />
             </article>
+            <img src={lightbulb} alt="Lightbulb Icon" /> */}
             <aside>
-                <section className="PromptTitle">
-                    <div className="ui-ux-title">The UI/UX Prompt</div>
+                <section className="PromptRight">
+                    <div className="infront-prompt"></div>
+                    <div className="behind-prompt"></div>
                 </section>
             </aside>
         </main>
