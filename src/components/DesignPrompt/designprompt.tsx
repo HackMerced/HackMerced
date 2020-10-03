77af63d58f1085f5810abdac8e920a29fb5c6877
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-03 00:38:09 -0700

    add: icon layers prompt

--- src/components/DesignPrompt/designprompt.tsx
+++ src/components/DesignPrompt/designprompt.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 
 import './designprompt.scss';
+import lightbulb from '../../assets/images/lightbulb.png';
 
 const DesignPrompt: FC = (): JSX.Element => {
     return (
         <main className="DESIGNPROMPT">
             <article>
-                <section className="left-prompt">
-                    <span className="circle"></span>
+                <section className="PromptLeft">
+                    <div id="one" className="circle"></div>
+                    <div id="two" className="circle"></div>
                 </section>
+                <img src={lightbulb} width="80%" alt="Lightbulb Icon" />
             </article>
             <aside>
                 <section className="PromptTitle">
