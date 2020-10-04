4b2ddf4058e17de159b23adadc98a50cd66b202f
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-04 14:48:03 -0700

    fix: about-designathon design

--- src/components/About Us Design/aboutus-design.tsx
+++ src/components/About Us Design/aboutus-design.tsx
@@ -1,29 +1,35 @@
 import React, { FC } from 'react';
 
-import TEAM from '../../assets/images/lightbulb-layers.png';
+import useWindowDimensions from '../WindowDimensions/WindowDimensions';
+import ABOUTICON from '../../assets/images/about-icon.png';
 import './aboutus-design.scss';
 
 const AboutUsDesign: FC = (): JSX.Element => {
+    const { width } = useWindowDimensions();
+
     return (
-        <section className="ABOUTUSDESIGN">
-            <h1> &emsp; About DesignMerced</h1>
-            <section className="about">
-                <article className="about-description">
-                    <div className="aboutUsText">
-                        <p>
-                            DesignMerced is HackMerced’s first virtual designathon for creatives and design individuals
-                            in high school and college from around the world. DesignMerced is a 24-hour event that will
-                            enhance designers/creators innovative skills and showcase their own uniqueness of solving
-                            art with technology. ‍<br></br>
-                            Students with all levels of design experience are encouraged to join us and discover their
-                            potential as problem-solvers, innovators, and future designers. Look forward to a day of
-                            workshops, mentorship, networking, games, and more!
-                        </p>
-                    </div>
+        <section className="about-us-design">
+            <div className="about-us-design__title">About DesignMerced</div>
+            <section className="about-us-design__content">
+                {width < 600 ? (
+                    <img src={ABOUTICON} alt="About Icon" className="about-us-design__content__logo" />
+                ) : null}
+                <article className="about-us-design__content__description">
+                    <div id="one__design__box" className="about-us-design__box"></div>
+                    <div id="two__design__box" className="about-us-design__box"></div>
+                    <p className="about-us-design__content__description__text">
+                        DesignMerced is HackMerced’s first virtual designathon for creatives and design individuals in
+                        high school and college from around the world. DesignMerced is a 24-hour event that will enhance
+                        designers/creators innovative skills and showcase their own uniqueness of solving design with
+                        technology. ‍<br></br> <br></br>
+                        Students with all levels of design experience are encouraged to join us and discover their
+                        potential as problem-solvers, innovators, and future designers. Look forward to a day of
+                        workshops, activities, networking, games, and more!
+                    </p>
                 </article>
-                <aside>
-                    <img src={TEAM} width="100%" alt="HackMerced Team" className="LOGO" />
-                </aside>
+                {width > 600 ? (
+                    <img src={ABOUTICON} alt="About Icon" className="about-us-design__content__logo" />
+                ) : null}
             </section>
         </section>
     );
