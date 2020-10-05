98a88ac2bc8a27a8351e2e25dc408a7d87386402
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-04 19:19:58 -0700

    fix: grammar errors

--- src/components/Title Design/title-design.tsx
+++ src/components/Title Design/title-design.tsx
@@ -1,19 +1,24 @@
 import React, { FC } from 'react';
 
-import Title from '../../assets/images/title.png';
-import Date from '../../assets/images/date.png';
-import TitleBox1 from '../../assets/images/paint-purple.jpeg';
+import DESIGNTITLE from '../../assets/images/title.png';
+import DESIGNDATE from '../../assets/images/date.png';
+import ABSTRACTPAINTING from '../../assets/images/paint-purple.jpeg';
 import './title-design.scss';
 
 const TitleDesign: FC = (): JSX.Element => {
     return (
-        <header className="TITLEDESIGN">
-            <section className="designmerced">
-                <img src={TitleBox1} width="600px" alt="DESIGNMERCED TITLE" className="image-square" />
-                <div className="background-square"></div>
-                <img src={Title} width="600px" alt="DESIGNMERCED TITLE" className="designmerced-title" />
-                <img src={Date} width="600px" alt="DESIGNMERCED DATE" className="designmerced-date" />
-                <div className="designmerced-subtext text-gray-100">24 hour designathon.</div>
+        <header className="TITLE__DESIGN">
+            <section className="TITLE__DESIGN__designmerced">
+                <img
+                    src={ABSTRACTPAINTING}
+                    width="600px"
+                    alt="DESIGNMERCED TITLE"
+                    className="TITLE__DESIGN__abstract__painting"
+                />
+                <div className="TITLE__DESIGN__background__square"></div>
+                <img src={DESIGNTITLE} width="600px" alt="DESIGNMERCED TITLE" className="TITLE__DESIGN__title" />
+                <img src={DESIGNDATE} width="600px" alt="DESIGNMERCED DATE" className="TITLE__DESIGN__date" />
+                <div className="designmerced-subtext text-gray-100">24-hour designathon.</div>
                 <div className="designmerced-text text-gray-100">HackMerced&apos;s first</div>
                 <button className="apply-button bg-white text-black font-bold">Apply Now</button>
             </section>
