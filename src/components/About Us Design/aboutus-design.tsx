d02c83ba0c6c99f471082b4a2d6e19092d2f9dfb
adriandarian <adarian@ucmerced.edu>
2020-10-06 23:34:05 -0700

    fix: oh god please let this be the last about us update

--- src/components/About Us Design/aboutus-design.tsx
+++ src/components/About Us Design/aboutus-design.tsx
@@ -15,13 +15,13 @@ const AboutUsDesign: FC = (): JSX.Element => {
                     <img src={ABOUT_ICON} alt="About Icon" className="about-us-design__content__logo" />
                 ) : null}
                 <article className="about-us-design__content__description">
-                    <div id="" className="about-us-design__content__description__box one__design__box"></div>
-                    <div id="" className="about-us-design__content__description__box two__design__box"></div>
+                    <div className="about-us-design__content__description__box one-design-box"></div>
+                    <div className="about-us-design__content__description__box two-design-box"></div>
                     <p className="about-us-design__content__description__text">
                         DesignMerced is UC Merced’s first virtual designathon, hosted by HackMerced for creatives and
                         design individuals in high school and college from around the world. DesignMerced is a 24-hour
                         event that will enhance designer&apos;s and creator&apos;s innovative skills and showcase their
-                        uniqueness of solving design with technology. ‍ <br></br>
+                        uniqueness of solving design with technology. <br></br>
                         <br></br>Students with all levels of design experience are encouraged to join us and discover
                         their potential as problem-solvers, innovators, and future designers. Look forward to a day of
                         workshops, activities, networking, games, and more!
