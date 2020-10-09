49cfd65a2b5b5911f2baffccc47fa0e5acd40325
Jatnael Montes <ifelsejet@gmail.com>
2020-10-09 13:43:35 -0700

    Links in pastWinners now open in a different tab

--- src/components/Past-Winners/past-winners.tsx
+++ src/components/Past-Winners/past-winners.tsx
@@ -21,52 +21,52 @@ const PastWinners: FC = (): JSX.Element => {
             <h3 className="past-winners-title">Past Winners</h3>
             <div className="break-line"></div>
             <nav className="winners">
-                <a href="https://devpost.com/software/helpers-spot">
+                <a href="https://devpost.com/software/helpers-spot" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faHandHoldingHeart} className="fa winner-icon" />
                     <h5 className="winner-title">Social Good</h5>
                     <h6 className="winner-group">Helpers Spot</h6>
                 </a>
-                <a href="https://devpost.com/software/vibe-xja5cd">
+                <a href="https://devpost.com/software/vibe-xja5cd" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faMedkit} className="fa winner-icon" />
                     <h5 className="winner-title">Health</h5>
                     <h6 className="winner-group">Vibe</h6>
                 </a>
-                <a href="https://devpost.com/software/kabobcat">
+                <a href="https://devpost.com/software/kabobcat" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faSeedling} className="fa winner-icon" />
                     <h5 className="winner-title">Sustainability</h5>
                     <h6 className="winner-group">Kabobcat</h6>
                 </a>
-                <a href="https://devpost.com/software/code-dude">
+                <a href="https://devpost.com/software/code-dude" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faPalette} className="fa winner-icon" />
                     <h5 className="winner-title">Best in Design</h5>
                     <h6 className="winner-group">Code-Dude</h6>
                 </a>
-                <a href="https://devpost.com/software/snapcode-vabpfn">
+                <a href="https://devpost.com/software/snapcode-vabpfn" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faUserPlus} className="fa winner-icon" />
                     <h5 className="winner-title">Best Beginner Hack</h5>
                     <h6 className="winner-group">Snapcode</h6>
                 </a>
-                <a href="https://devpost.com/software/esose">
+                <a href="https://devpost.com/software/esose" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faSearch} className="fa winner-icon" />
                     <h5 className="winner-title">Best Product Research</h5>
                     <h6 className="winner-group">ESOSE</h6>
                 </a>
-                <a href="https://devpost.com/software/3d-for-education">
+                <a href="https://devpost.com/software/3d-for-education" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faMicrochip} className="fa winner-icon" />
                     <h5 className="winner-title">Best Hardware Hack</h5>
                     <h6 className="winner-group">3D for Education</h6>
                 </a>
-                <a href="https://devpost.com/software/ghym">
+                <a href="https://devpost.com/software/ghym" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faMeteor} className="fa winner-icon" />
                     <h5 className="winner-title">Moonshot</h5>
                     <h6 className="winner-group">Ghym</h6>
                 </a>
-                <a href="https://devpost.com/software/a-clear-space">
+                <a href="https://devpost.com/software/a-clear-space" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faDigitalOcean} className="fa winner-icon" />
                     <h5 className="winner-title">Best Use of Digital Ocean</h5>
                     <h6 className="winner-group">A Clean Space</h6>
                 </a>
-                <a href="https://devpost.com/software/project-dms">
+                <a href="https://devpost.com/software/project-dms" target="_blank"  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faCloud} className="fa winner-icon" />
                     <h5 className="winner-title">Best Use of Google Cloud</h5>
                     <h6 className="winner-group">Project DMS</h6>
