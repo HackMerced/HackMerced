b5e5f6b4df88ebb80ed2a236e8c178e8745f415d
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-11 01:23:56 -0700

    fix: change subscribe to designmerced button

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -67,9 +67,12 @@ const Title: FC = (): JSX.Element => {
                 <button
                     className="subscribe-button bg-white text-black font-bold"
                     // className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                    onClick={openModal}
+                    onClick={() => {
+                        if (false) openModal();
+                        window.location.href = '/#designmerced';
+                    }}
                 >
-                    Subscribe
+                    DesignMerced
                 </button>
             </section>
             {showModal ? (
