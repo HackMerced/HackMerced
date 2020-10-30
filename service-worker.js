66e3502c0ec842be60248a8f65c506a9b96825cf
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-10-30 15:38:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4535cc59623941dbba597465f93d6e7e.js"
+  "/precache-manifest.13a6250d494d3e32efa21cc00b21213f.js"
 );
 
 self.addEventListener('message', (event) => {
