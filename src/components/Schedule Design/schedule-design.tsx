fb4b0259948194b56c5e7cbad098959563df8cc2
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-11-12 13:51:05 -0800

    add: updated schedule

--- src/components/Schedule Design/schedule-design.tsx
+++ src/components/Schedule Design/schedule-design.tsx
@@ -19,35 +19,55 @@ const ScheduleDesign: FC = (): JSX.Element => {
 
                                 <div className="event_item">
                                     <div className="event_item_dot dot_active"></div>
-                                    <div className="event_item_title">9:30 AM</div>
+                                    <div className="event_item_title">9:00 AM - 10:00 AM</div>
                                     <div className="event_item_body">Opening Ceremony</div>
                                 </div>
-
                                 <div className="event_item">
                                     <div className="event_item_dot"></div>
-                                    <div className="event_item_title">12:00 PM</div>
-                                    <div className="event_item_body">Bob Ross Activity</div>
+                                    <div className="event_item_title">10:00 AM</div>
+                                    <div className="event_item_body">Hacking Starts & <br></br>Team Building Activity </div>
                                 </div>
 
                                 <div className="event_item">
                                     <div className="event_item_dot"></div>
-                                    <div className="event_item_title">9:30 AM</div>
+                                    <div className="event_item_title">11:00 AM - 12:00 PM</div>
+                                    <div className="event_item_body"> Introduction to Adobe Workshop</div>
+                                </div>
+
+                                <div className="event_item">
+                                    <div className="event_item_dot"></div>
+                                    <div className="event_item_title">1:00 PM - 2:00 PM</div>
                                     <div className="event_item_body">
-                                        Hacking Ends
+                                        Beginner Figma Workshop
                                         {/* <span className="contact">Location</span> */}
                                     </div>
                                 </div>
 
                                 <div className="event_item">
                                     <div className="event_item_dot"></div>
-                                    <div className="event_item_title">10:00 AM</div>
-                                    <div className="event_item_body">Judging</div>
+                                    <div className="event_item_title">3:00 PM - 4:00 PM</div>
+                                    <div className="event_item_body">Adobe Researching Design</div>
                                 </div>
 
                                 <div className="event_item">
                                     <div className="event_item_dot"></div>
-                                    <div className="event_item_title">12:00 PM</div>
-                                    <div className="event_item_body">Closing Ceremony</div>
+                                    <div className="event_item_title">4:00 PM - 5:00 PM</div>
+                                    <div className="event_item_body">Minecraft Activity</div>
+                                </div>
+                                <div className="event_item">
+                                    <div className="event_item_dot"></div>
+                                    <div className="event_item_title">6:00 PM - 7:00 PM</div>
+                                    <div className="event_item_body">Intro to UI/UX and Design Careers</div>
+                                </div>
+                                <div className="event_item">
+                                    <div className="event_item_dot"></div>
+                                    <div className="event_item_title">8:00 PM - 9:00 PM</div>
+                                    <div className="event_item_body">Beginner Product Design</div>
+                                </div>
+                                <div className="event_item">
+                                    <div className="event_item_dot"></div>
+                                    <div className="event_item_title">11:00 PM - 12:00 PM</div>
+                                    <div className="event_item_body">How to Prototype/Wireframe</div>
                                 </div>
                             </div>
                         </div>
