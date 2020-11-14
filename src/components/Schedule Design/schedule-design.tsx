164e721ad7e36bbdf0889f9e6915550575a4a31a
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-11-14 14:46:21 -0800

    add: another day for schedule

--- src/components/Schedule Design/schedule-design.tsx
+++ src/components/Schedule Design/schedule-design.tsx
@@ -1,8 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import './schedule-design.scss';
+import { schedule } from './schedule';
 
 const ScheduleDesign: FC = (): JSX.Element => {
+    const [day, setDay] = useState(21);
+
+    const generateSchedule = (): any => {
+        let scheduleElements: any = [];
+
+        schedule[day].forEach((e: any) =>
+            scheduleElements.push(
+                <div className="event_item">
+                    <div className="event_item_dot dot_active"></div>
+                    <div className="event_item_title">{e.title}</div>
+                    <div className="event_item_body">{e.body}</div>
+                </div>,
+            ),
+        );
+
+        return scheduleElements;
+    };
+
     return (
         <main className="ScheduleDesign">
             <section className="schedule-designathon">
@@ -11,64 +30,20 @@ const ScheduleDesign: FC = (): JSX.Element => {
                         <div className="calendar">
                             <div className="calendar_header">
                                 <div className="header_title">Schedule</div>
-                                <p className="calendar_subheader">November 21, 2020</p>
+                                <div className="side-by-side">
+                                    <p className="calendar_subheader" onClick={() => setDay(21)}>
+                                        November 21
+                                    </p>
+                                    <p className="calendar_subheader" onClick={() => setDay(22)}>
+                                        November 22
+                                    </p>
+                                </div>
                             </div>
 
                             <div className="calendar_events">
                                 {/* <p className="calendar_events_title">November 21, 2020</p> */}
 
-                                <div className="event_item">
-                                    <div className="event_item_dot dot_active"></div>
-                                    <div className="event_item_title">9:00 AM - 10:00 AM</div>
-                                    <div className="event_item_body">Opening Ceremony</div>
-                                </div>
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">10:00 AM</div>
-                                    <div className="event_item_body">Hacking Starts & <br></br>Team Building Activity </div>
-                                </div>
-
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">11:00 AM - 12:00 PM</div>
-                                    <div className="event_item_body"> Introduction to Adobe Workshop</div>
-                                </div>
-
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">1:00 PM - 2:00 PM</div>
-                                    <div className="event_item_body">
-                                        Beginner Figma Workshop
-                                        {/* <span className="contact">Location</span> */}
-                                    </div>
-                                </div>
-
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">3:00 PM - 4:00 PM</div>
-                                    <div className="event_item_body">Adobe Researching Design</div>
-                                </div>
-
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">4:00 PM - 5:00 PM</div>
-                                    <div className="event_item_body">Minecraft Activity</div>
-                                </div>
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">6:00 PM - 7:00 PM</div>
-                                    <div className="event_item_body">Intro to UI/UX and Design Careers</div>
-                                </div>
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">8:00 PM - 9:00 PM</div>
-                                    <div className="event_item_body">Beginner Product Design</div>
-                                </div>
-                                <div className="event_item">
-                                    <div className="event_item_dot"></div>
-                                    <div className="event_item_title">11:00 PM - 12:00 PM</div>
-                                    <div className="event_item_body">How to Prototype/Wireframe</div>
-                                </div>
+                                {generateSchedule()}
                             </div>
                         </div>
                     </div>
---
+++ src/components/Schedule Design/schedule.ts
@@ -0,0 +1,75 @@
+export const schedule: any = {
+    21: [
+        {
+            title: "9:00 AM - 10:00 AM",
+            body: "Opening Ceremony"
+        },
+        {
+            title: "10:00 AM",
+            body: "Designing Starts & Team Building Activity"
+        },
+        {
+            title: "11:00 AM - 12:00 PM",
+            body: "Introduction to Adobe Workshop"
+        },
+        {
+            title: "1:00 PM - 2:00 PM",
+            body: "Beginner Figma Workshop"
+        },
+        {
+            title: "3:00 PM - 4:00 PM",
+            body: "Adobe Researching Design"
+        },
+        {
+            title: "4:00 PM - 5:00 PM",
+            body: "Minecraft Activity"
+        },
+        {
+            title: "6:00 PM - 7:00 PM",
+            body: "Intro to UI/UX and Design Careers"
+        },
+        {
+            title: "8:00 PM - 9:00 PM",
+            body: "Beginner Product Design"
+        },
+        {
+            title: "11:00 PM - 12:00 AM",
+            body: "How to Prototype/Wireframe"
+        },
+    ],
+    22: [
+        {
+            title: "12:00 AM - 1:00 AM",
+            body: "Among Us Game",
+           
+        },
+        {
+            title: "1:00 AM - 2:00 AM",
+            body: "Brainstorming activity"
+        },
+        {
+            title: "3:00 AM - 4:00 AM",
+            body: "Losing My Marbles"
+        },
+        {
+            title: "5:00 AM - 6:00 AM",
+            body: "Skribble.io Game"
+        },
+        {
+            title: "7:00 AM - 8:00 AM",
+            body: "JackBox"
+        },
+        {
+            title: "9:00 AM",
+            body: "Designing Ends"
+        },
+        {
+            title: "10:00 AM - 12:00 PM",
+            body: "Judging"
+        },
+        {
+            title: "12:00 PM - 1:00 PM",
+            body: "Closing Ceremony"
+        },
+    ]
+}
