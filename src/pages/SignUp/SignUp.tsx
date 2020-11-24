fd7c8c03917462b1bab4f098a959a050e70a3b03
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-11-24 15:08:00 -0800

    fix: login/signup buttons

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -80,7 +80,10 @@ const SignUp: FC = (): JSX.Element => {
                             </h2>
                         </div>
 
-                        <input className="submitbutton" type="submit" value="Create an Account"></input>
+                        <button className="submitbutton" type="submit" value="Create an Account">
+                        <Link to="/dashboard"><a>Create an Account</a></Link>    
+                                   
+                        </button>
                     </form>
 
                     {/* <div className="loginicons">
