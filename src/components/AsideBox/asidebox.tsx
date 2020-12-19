4671e4fb2b1bacf64d52dfb80ffacfbc962810a6
AlissonRoss <alirosssalazar@gmail.com>
2020-12-19 13:51:47 -0800

    Fixed broken application link within the subnavbar and some additions

---
+++ src/components/AsideBox/asidebox.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import './asidebox.scss'
+
+const AsideBox: FC = (): JSX.Element => {
+    return (
+        <>
+            <div className="item-box">
+            </div>
+        </>
+    );
+};
+export default AsideBox;
