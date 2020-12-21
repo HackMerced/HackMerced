a7c127477f44d82e8f32a342eecee55589469c7b
adriandarian <adarian@ucmerced.edu>
2020-12-20 21:17:03 -0800

    setup environments

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,30 +1,28 @@
-import React, { FC } from 'react';
-// import Axios from 'axios';
+import React, { FC, useState } from 'react';
+import Axios from 'axios';
 import { Link } from 'react-router-dom';
+
 import Footer from '../../components/Footer/footer';
 import Navbar from '../../components/NavBar/navbar';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFacebook, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import Hackmercedlogo from '../../assets/images/hackmercedvi-logo.png';
-
 import './SignUp.scss';
+import { environment } from '../../environments';
 
 const SignUp: FC = (): JSX.Element => {
-    // const[form, setForm] = useState({email:"", password:""});
-    // let { pathname } = useLocation();
+    const [form, setForm] = useState({ email: '', password: '' });
 
-    // const handleInputChange = (
-    //     event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
-    // ): void => {
-    //     const { name, value } = event.target;
-    //     setForm({ ...form, [name]: value });
-    // };
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
+    ): void => {
+        const { name, value } = event.target;
+        setForm({ ...form, [name]: value });
+    };
 
-    // const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
-    //     event.preventDefault();
-    //     console.log(form);
-    //     Axios.post(`https://hackmerced-tomoe.herokuapp.com/v1/auth/login`, form).then(Response => console.log(Response));
-    // }
+    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        console.log(`${environment.HACKER_API}/v1/auth/register`);
+        Axios.post(`${environment.HACKER_API}/v1/auth/register`, form).then(Response => console.log(Response));
+    };
 
     return (
         <>
@@ -32,9 +30,8 @@ const SignUp: FC = (): JSX.Element => {
             <main className="sign-page">
                 <div className="center-container">
                     <img className="logo" src={Hackmercedlogo} alt="Logo" height={120} width={120} />
-                    <h1>SIGN UP</h1>
-
-                    <form>
+                    <h1 className="uppercase">Sign Up</h1>
+                    <form onSubmit={handleSubmit}>
                         <input
                             className="firstinput"
                             id="email"
@@ -42,8 +39,8 @@ const SignUp: FC = (): JSX.Element => {
                             type="text"
                             placeholder="First Name"
                             required
+                            onChange={handleInputChange}
                         />
-
                         <input
                             className="firstinput"
                             id="email"
@@ -51,47 +48,40 @@ const SignUp: FC = (): JSX.Element => {
                             type="text"
                             placeholder="Last Name"
                             required
+                            onChange={handleInputChange}
                         />
-                        <input className="emailinput" type="email" placeholder="Email" id="email" name="email"></input>
-
+                        <input
+                            className="emailinput"
+                            type="email"
+                            placeholder="Email"
+                            id="email"
+                            name="email"
+                            onChange={handleInputChange}
+                        />
                         <input
                             className="passwordinput"
                             type="password"
                             placeholder="Password"
                             id="password"
                             name="password"
-                        ></input>
-                        <br></br>
+                            onChange={handleInputChange}
+                        />
+                        <br />
                         <div className="flex-container">
                             <input type="checkbox" id="rememberme" name="remember" />
                             <h2 className="flex-item">
                                 <h3>Remember Me?</h3>
-
-                                {/* {pathname === '/login-resetpassword'} */}
                             </h2>
                             <h2 className="flex-item">
-                                <button type="button">
-                                    <Link to="/Login">
-                                        <u>Already have an Account?</u>
-                                    </Link>{' '}
-                                    {/* NEED TO LINK TO SIGN-UP PAGE */}
-                                    {/* {pathname === '/login-resetpassword'}                               THESE ARE PLACEHOLDERS */}
-                                </button>
+                                <Link to="/Login">
+                                    <u>Already have an Account?</u>
+                                </Link>
                             </h2>
                         </div>
-
-                        <button className="submitbutton" type="submit" value="Create an Account">
-                        <Link to="/dashboard"><a>Create an Account</a></Link>    
-                                   
+                        <button className="submitbutton" type="submit">
+                            Create an Account
                         </button>
                     </form>
-
-                    {/* <div className="loginicons">
-                        <p>Or Sign Up with</p>
-                        <FontAwesomeIcon icon={faFacebook} className="facebookicon" />
-                        <FontAwesomeIcon icon={faGoogle} className="googleicon" />
-                        <FontAwesomeIcon icon={faGithub} className="githubicon" />
-                    </div> */}
                 </div>
             </main>
             <Footer />
