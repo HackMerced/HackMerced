fedb34c74d0091f62f42af1e019bdc6a116e38b7
adriandarian <adarian@ucmerced.edu>
2020-12-20 21:17:03 -0800

    setup environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment_dev = {
+  production: false,
+  HACKER_API: "http://localhost:3001"
+};
--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,19 +1,18 @@
 import React, { FC, useState } from 'react';
 import Axios from 'axios';
-import { Link, useLocation } from 'react-router-dom';
-import Footer from '../../components/Footer/footer';
-import Navbar from '../../components/NavBar/navbar';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFacebook, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faKey, faUser } from '@fortawesome/free-solid-svg-icons';
 
 import Hackmercedlogo from '../../assets/images/hackmercedvi-logo.png';
+import Footer from '../../components/Footer/footer';
+import Navbar from '../../components/NavBar/navbar';
+import { environment } from '../../environments';
 
 import './login.scss';
 
 const Login: FC = (): JSX.Element => {
-    const[form, setForm] = useState({email:"", password:""});
-    let { pathname } = useLocation();
+    const [form, setForm] = useState({ email: '', password: '' });
 
     const handleInputChange = (
         event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
@@ -24,9 +23,10 @@ const Login: FC = (): JSX.Element => {
 
     const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
         event.preventDefault();
-        console.log(form);
-        Axios.post(`https://hackmerced-tomoe.herokuapp.com/v1/auth/login`, form).then(Response => console.log(Response));
-    }
+        Axios.post(`${environment.HACKER_API}/v1/auth/login`, form).then(Response =>
+            console.log(Response),
+        );
+    };
 
     return (
         <>
@@ -34,46 +34,50 @@ const Login: FC = (): JSX.Element => {
             <main className="login-page">
                 <div className="center-container">
                     <img className="logo" src={Hackmercedlogo} alt="Logo" height={120} width={120} />
-                    <h1>LOGIN</h1>
-
+                    <h1 className="uppercase">log in</h1>
                     <form onSubmit={handleSubmit}>
-                        <input onChange={handleInputChange} className="emailinput" type="email" placeholder="Email" id="email" name="email"></input>
+                        <input
+                            onChange={handleInputChange}
+                            className="emailinput"
+                            type="email"
+                            placeholder="Email"
+                            id="email"
+                            name="email"
+                        />
                         <FontAwesomeIcon icon={faUser} className="blackicon" />
-                        <input onChange={handleInputChange}
+                        <input
+                            onChange={handleInputChange}
                             className="passwordinput"
                             type="password"
                             placeholder="Password"
                             id="password"
                             name="password"
-                        ></input>
+                        />
                         <FontAwesomeIcon icon={faKey} className="blackicon" />
-
                         <div className="flex-container">
                             <h2 className="flex-item">
-                                <button type="button">
-                                    <Link to="/login-resetpassword"><u>Forgot Password?</u></Link>
-                                    {pathname === '/login-resetpassword'}
-                                </button>
+                                <Link
+                                    to={{
+                                        pathname: '/login',
+                                        hash: '#reset-password',
+                                        state: { fromLogin: true },
+                                    }}
+                                >
+                                    <u>Forgot Password?</u>
+                                </Link>
                             </h2>
                             <h2 className="flex-item">
-                                <button type="button">
-                                    <Link to="/SignUp"><u>Create an Account</u></Link>     {/* NEED TO LINK TO SIGN-UP PAGE */}
-                                    {pathname === '/SignUp'}                               {/* THESE ARE PLACEHOLDERS */}
-                                </button>
+                                <Link to="/SignUp">
+                                    <u>Create an Account</u>
+                                </Link>
                             </h2>
                         </div>
-
-                        <button className="submitbutton" type="submit" value="LOG IN">
-                         <Link to="/dashboard"><a>LOG IN</a></Link>    
-                                    {pathname === '/dashboard'} </button>
+                        <button className="submitbutton" type="submit">
+                            <Link to="/dashboard" className="uppercase">
+                                Log in
+                            </Link>
+                        </button>
                     </form>
-
-                    {/* <div className="loginicons">
-                        <p>Or login with</p>
-                        <FontAwesomeIcon icon={faFacebook} className="facebookicon" />
-                        <FontAwesomeIcon icon={faGoogle} className="googleicon" />
-                        <FontAwesomeIcon icon={faGithub} className="githubicon" />
-                    </div> */}
                 </div>
             </main>
             <Footer />
