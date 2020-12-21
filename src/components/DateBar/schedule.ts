cb574c1a06ee774ce373680219a8144c239a2a91
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-12-20 19:56:39 -0800

    Made mockup dates & displayed them for hackathon

---
+++ src/components/DateBar/schedule.ts
@@ -0,0 +1,32 @@
+export const schedule: any = {
+    3: [
+        {
+            title: "9:00 AM - 10:00 AM",
+            body: "Opening Ceremony"
+        },
+        {
+            title: "10:00 AM",
+            body: "Hacking Begins"
+        },
+    ],
+    4: [
+        {
+            title: "12:00 AM - 1:00 AM",
+            body: "Lunch Break", 
+        },
+        {
+            title: "1:00 AM - 2:00 AM",
+            body: "Brainstorming activity"
+        },
+    ],
+    5: [
+        {
+            title: "12:00 AM - 1:00 AM",
+            body: "Among Us Game", 
+        },
+        {
+            title: "1:00 AM - 2:00 AM",
+            body: "Closing Ceremony"
+        },
+    ]
+}
