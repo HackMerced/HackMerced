b9f7f37d3ffad29aa5f05743de9ce2b94682b6e8
adriandarian <adarian@ucmerced.edu>
2020-12-20 21:17:03 -0800

    setup environments

---
+++ src/environments/index.ts
@@ -0,0 +1,4 @@
+import { environment_dev } from './environment.dev';
+import { environment_prod } from './environment.prod';
+
+export const environment = process.env.NODE_ENV === 'production' ? environment_prod : environment_dev;
