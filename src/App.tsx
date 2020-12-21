f746e96340488679b981eade6af31016bae333fb
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-12-20 19:54:56 -0800

    added route to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import SignUp from './pages/SignUp/SignUp';
 import HackMercedVI from './pages/HackMercedVI/HackMercedVI';
 import DashBoard from './pages/Dashboard/dashboard';
 import DashboardPrizes from './pages/Dashboard-Prizes/dashboard-prizes';
+import DashboardSchedule from './pages/Dashboard-Schedule/dashboard-schedule';
 
 import './App.scss';
 
@@ -34,6 +35,7 @@ const App: FC = (): JSX.Element => {
                 <Route exact path="/hackmercedvi" component={HackMercedVI} />
                 <Route exact path="/dashboard" component={DashBoard} />
                 <Route exact path="/dashboard-prizes" component={DashboardPrizes} />
+                <Route exact path="/dashboard-schedule" component={DashboardSchedule} />
                 <Route path="*" component={Error} />
                 <Redirect from="/HackMerced" to="/" />
             </Switch>
