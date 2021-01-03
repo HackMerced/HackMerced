c7f6e93ed04344336375df5e4d4bac3e3f6a0f36
Adrian Darian <adarian@ucmerced.edu>
2021-01-02 23:27:48 -0800

    fix: aside boxes, team code input, countdown responsiveness

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,8 @@
-import React, { FC, Fragment, useState, useEffect, useRef } from "react";
+import React, { FC, Fragment, useState } from "react";
 import { Link, useLocation, useHistory } from "react-router-dom";
+import Axios, { AxiosResponse } from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 import Footer from "../../components/Footer";
 import Navbar from "../../components/NavBar";
@@ -9,6 +12,7 @@ import HACKMERCED_LOGO from "../../assets/images/hackmerced-logo.png";
 import Live from "./live";
 import Schedule from "./schedule";
 import Prizes from "./prizes";
+import { environment } from "../../environments";
 
 const authenticateHacker = (hacker: HackerState) => {
     if (hacker.id === "" || hacker.email === "" || hacker.firstName === "" || hacker.lastName === "") {
@@ -33,32 +37,16 @@ const Dashboard: FC<{
     updateToken: React.Dispatch<React.SetStateAction<TokenState | undefined>>;
 }> = ({ hacker, token }): JSX.Element => {
     const { pathname } = useLocation();
-    const [openDrawer, toggleDrawer] = useState(false);
-    const drawerRef = useRef(null);
+    const [code, setCode] = useState<string>("");
     const history = useHistory();
-
-    useEffect(() => {
-        const closeDrawer = (event: { target: any }) => {
-            if (drawerRef.current && (drawerRef?.current as any).contains(event.target)) {
-                return;
-            }
-
-            toggleDrawer(false);
-        };
-
-        document.addEventListener("mousedown", closeDrawer);
-        return () => document.removeEventListener("mousedown", closeDrawer);
-    }, []);
 
     if (hacker === undefined || token === undefined) {
         // history.push("/login");
     } else if (!authenticateHacker(hacker as HackerState) && !authenticateToken(token as TokenState)) {
         history.push("/401");
+    } else {
+        setCode(hacker?.teamCode);
     }
-
-    const SubNavbarItemsProps = {
-        openDrawer: openDrawer,
-    };
 
     const BreakLine = (): JSX.Element => {
         return <div className="break-line"></div>;
@@ -77,13 +65,38 @@ const Dashboard: FC<{
         }
     };
 
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setCode(event.target.value);
+    };
+
+    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        Axios.post(
+            `${environment.HACKER_API}/v1/auth/login`,
+            {
+                hacker: hacker?.id,
+                teamCode: code,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            },
+        ).then((response: AxiosResponse) => {
+            if (response.status === 201 && response.statusText === "CREATED") {
+                // updateHacker({ ...hacker, teamCode: code });
+                console.table({ ...hacker, teamCode: code });
+            }
+        });
+    };
+
     return (
         <Fragment>
             <Navbar showBanner={false} />
             <main className="dashboard">
                 <header className="dashboard__sub-navbar">
                     <nav className="dashboard__sub-navbar__nav">
-                        <ul className="dashboard__sub-navbar__nav__ul" ref={drawerRef} {...SubNavbarItemsProps}>
+                        <ul className="dashboard__sub-navbar__nav__ul">
                             <li className="dashboard__sub-navbar__nav__ul__li">
                                 <Link to="/dashboard" className="dashboard__sub-navbar__nav__ul__li__title">
                                     LIVE
@@ -106,21 +119,32 @@ const Dashboard: FC<{
                     </nav>
                 </header>
                 <section className="dashboard__content">{getDashboardContent()}</section>
-                <section className="dashboard__event">
-                    <img className="dashboard__event__logo" src={HACKMERCED_LOGO} alt="Logo" />
-                    <h1 className="dashboard__event__title">HackMerced VI</h1>
-                    <h2 className="dashboard__event__start-title">Hacking Starts</h2>
-                    <h3 className="dashboard__event__start-time">March 5, 2021</h3>
-                    <h2 className="dashboard__event__end-title">Hacking Ends</h2>
-                    <h3 className="dashboard__event__end-time">March 7, 2021</h3>
-                </section>
-                <section className="dashboard__team">
-                    <h1 className="dashboard__team__title">Team Code</h1>
-                    <h3 className="dashboard__team__code">fkjlash</h3>
-                    <p className="dashboard__team__break">- or -</p>
-                    <h2 className="dashboard__team__code-title">Enter Team Code</h2>
-                    <input className="dashboard__team__code-input" type="text" id="codeInput" name="password" />
-                </section>
+                <aside className="dashboard__aside">
+                    <section className="dashboard__aside__event">
+                        <img className="dashboard__aside__event__logo" src={HACKMERCED_LOGO} alt="Logo" />
+                        <h1 className="dashboard__aside__event__title">HackMerced VI</h1>
+                        <h2 className="dashboard__aside__event__start-title">Hacking Starts</h2>
+                        <h3 className="dashboard__aside__event__start-time">March 5, 2021</h3>
+                        <h2 className="dashboard__aside__event__end-title">Hacking Ends</h2>
+                        <h3 className="dashboard__aside__event__end-time">March 7, 2021</h3>
+                    </section>
+                    <section className="dashboard__aside__team">
+                        <h1 className="dashboard__aside__team__title">Team Code</h1>
+                        <h3 className="dashboard__aside__team__code">{code === "" ? "No Team" : code}</h3>
+                        <p className="dashboard__aside__team__break">- or -</p>
+                        <h2 className="dashboard__aside__team__code-title">Enter Team Code</h2>
+                        <form className="dashboard__aside__team__code-container" onSubmit={handleSubmit}>
+                            <input
+                                className="dashboard__aside__team__code-container__input"
+                                type="text"
+                                onChange={handleInputChange}
+                            />
+                            <button className="dashboard__aside__team__code-container__submit" type="submit">
+                                <FontAwesomeIcon icon={faPlay} className="blackicon" />
+                            </button>
+                        </form>
+                    </section>
+                </aside>
             </main>
             <Footer />
         </Fragment>
