d656a8fde76c74f6e19df1c4be28897597dffd35
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2021-01-29 14:35:19 -0800

    add: faq component

--- src/pages/HackMercedVI/HackMercedVI.tsx
+++ src/pages/HackMercedVI/HackMercedVI.tsx
@@ -1,14 +1,15 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
-import Navbar from "../../components/NavBar/navbar";
-import Footer from "../../components/Footer/footer";
-import Landing from "../../components/LandingHMVI/landinghmvi";
-
+import Navbar from '../../components/NavBar/navbar';
+import Footer from '../../components/Footer/footer';
+import Landing from '../../components/LandingHMVI/landinghmvi';
+import FAQ from '../../components/HMVIFAQ/HMVIFAQ';
 const HackMercedVI: FC = (): JSX.Element => {
     return (
         <main>
             <Navbar backgroundColor="#B63F1A" textColor="#FFFFF" breakLineColor="#C4BDDC" showBanner={false} />
             <Landing />
+            <FAQ />
             <Footer backgroundColor="#B63F1A" textColor="#FFFFF" fontColor="#EEEBF5" />
         </main>
     );
