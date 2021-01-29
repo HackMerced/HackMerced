3a705bf35a8a97dd218f54b7050faa85fc03cb55
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2021-01-29 13:36:30 -0800

    add: landing component

--- src/pages/HackMercedVI/HackMercedVI.tsx
+++ src/pages/HackMercedVI/HackMercedVI.tsx
@@ -1,27 +1,16 @@
-import React, { FC } from 'react';
-// import Axios from 'axios';
-// import { Link } from 'react-router-dom';
-import Footer from '../../components/Footer/footer';
-import Navbar from '../../components/NavBar/navbar';
+import React, { FC } from "react";
 
-import './HackMercedVI.scss';
+import Navbar from "../../components/NavBar/navbar";
+import Footer from "../../components/Footer/footer";
+import Landing from "../../components/LandingHMVI/landinghmvi";
 
 const HackMercedVI: FC = (): JSX.Element => {
     return (
-        <>
-            <Navbar backgroundColor="#E0E5EC" textColor="#0d0721" breakLineColor="#C4BDDC" showBanner={false}/>
-            <section className="version-six">
-                <div className="back">
-                    <a  className="text">
-                        HackMerced VI
-                    </a>
-                    <a  className="text2">
-                        Coming Soon in Spring 2021
-                    </a>
-                </div>
-            </section>
-            <Footer backgroundColor="#E0E5EC" textColor="#0d0721" fontColor="#0d0721" />
-        </>
+        <main>
+            <Navbar backgroundColor="#B63F1A" textColor="#FFFFF" breakLineColor="#C4BDDC" showBanner={false} />
+            <Landing />
+            <Footer backgroundColor="#B63F1A" textColor="#FFFFF" fontColor="#EEEBF5" />
+        </main>
     );
 };
 
