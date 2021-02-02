e83f1a1b74ab6bc4841815ab2ac0ee3d905ae1a9
Truman Chan <trumanjfchan@gmail.com>
2021-02-01 21:08:20 -0800

    Created Themes Component - HackMerced VI
    Created the Themes Component for our HackMerced VI page. Fully responsive.

--- src/pages/HackMercedVI/HackMercedVI.tsx
+++ src/pages/HackMercedVI/HackMercedVI.tsx
@@ -3,12 +3,14 @@ import React, { FC } from 'react';
 import Navbar from '../../components/NavBar/navbar';
 import Footer from '../../components/Footer/footer';
 import Landing from '../../components/LandingHMVI/landinghmvi';
+import Themes from '../../components/Themes/themes';
 import FAQ from '../../components/HMVIFAQ/HMVIFAQ';
 const HackMercedVI: FC = (): JSX.Element => {
     return (
         <main>
             <Navbar backgroundColor="#B63F1A" textColor="#FFFFF" breakLineColor="#C4BDDC" showBanner={false} />
             <Landing />
+            <Themes />
             <FAQ />
             <Footer backgroundColor="#B63F1A" textColor="#FFFFF" fontColor="#EEEBF5" />
         </main>
