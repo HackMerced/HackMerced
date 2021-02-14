fa2508dd5d5a5beb3a31f30c95344e5c9523ab6b
adriandarian <adarian@ucmerced.edu>
2021-02-13 18:49:28 -0800

    fix: event page responsiveness

--- src/components/HMVIFAQ/HMVIFAQ.tsx
+++ src/components/HMVIFAQ/HMVIFAQ.tsx
@@ -7,7 +7,7 @@ import './HMVIFAQ.scss';
 const HMVIFAQ: FC = () => {
     return (
         <section className="hackmercedvi-faq">
-            <h2 className="hackmerced-faq__title">Frequently Asked Questions</h2>
+            <h2 className="hackmercedvi-faq__title">Frequently Asked Questions</h2>
             <Accordian data={FAQData} />
         </section>
     );
