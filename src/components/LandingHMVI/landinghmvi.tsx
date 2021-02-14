d1f2034c54ed4fa35dd8f3000cec53be2591fdc9
Mary <mdelosreyes3@ucmerced.edu>
2021-02-14 14:50:39 -0800

    fix: responsiveness for event page

--- src/components/LandingHMVI/landinghmvi.tsx
+++ src/components/LandingHMVI/landinghmvi.tsx
@@ -1,12 +1,26 @@
 import React, { FC } from 'react';
 
-import LANDING_DESIGN from '../../assets/images/landing-page-hmvi.png';
+import useWindowDimensions from '../WindowDimensions/WindowDimensions';
+import './landinghmvi.scss';
 
 const LandingHMVI: FC = (): JSX.Element => {
+    const { width, height } = useWindowDimensions();
+    console.log(`width: ${width}, height: ${height}`);
+    let backgroundHeight = height > 600 ? height / 2 : 400;
+    backgroundHeight = width > 800 ? height + height / 3 - 50 : backgroundHeight;
     return (
-        <main className="LandingDesign">
-            <section className="hackathon-title">
-                <img src={LANDING_DESIGN} width="100%" alt="Landing Page HackMerced VI" />
+        <main className="LandingDesign" style={{ width: width, height: backgroundHeight }}>
+            <section className="LandingDesign__container">
+                <button
+                    className="LandingDesign__container__button bg-white text-black font-bold"
+                    onClick={() => {
+                        window.location.href = 'https://hackmercedvi.typeform.com/to/mY7E1Fug';
+                    }}
+                >
+                    <h1 className="LandingDesign__container__button__title">
+                        Register <br /> Date: March 12-14
+                    </h1>
+                </button>
             </section>
         </main>
     );
