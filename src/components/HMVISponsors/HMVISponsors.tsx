4530428cd98ecf40baf3572579d342abb20ff330
Adrian Darian <adarian@ucmerced.edu>
2021-03-03 15:00:59 -0800

    fix: stickermule logo

--- src/components/HMVISponsors/HMVISponsors.tsx
+++ src/components/HMVISponsors/HMVISponsors.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import SponsorHackMercedVI from '../../assets/images/sponsors-hackmercedvi.png';
+import STICKER_MULE from '../../assets/images/sticker-mule-logo-light.png';
 import './HMVISponsors.scss';
 
 const HMVISponsors: FC = (): JSX.Element => {
@@ -8,6 +9,7 @@ const HMVISponsors: FC = (): JSX.Element => {
             <section className="sponsor-hackmercedvi-title">
                 <h2>Sponsors</h2>
                 <img src={SponsorHackMercedVI} width="100%" alt="Our Sponsors for HackMerced VI" />
+                <a className="sticker_mule" href="http://hackp.ac/mlh-stickermule-hackathons"><img className="sticker_mule__img" src={STICKER_MULE} /></a>
             </section>
         </main>
     );
