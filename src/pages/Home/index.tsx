090e17de05c83d9bbe08c58ec75a7a40c3824432
Adrian Darian <adarian@ucmerced.edu>
2021-03-21 14:45:35 -0700

    fix: winners to be dynamic and added some new classNames

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,6 @@
 import React, { FC } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faHandHoldingHeart,
-    faHandshake,
-    faLaptopCode,
-    faPalette,
-    faMeteor,
-    faSchool,
-    faMicrochip,
-    faSearch,
-    faCloud,
-} from "@fortawesome/free-solid-svg-icons";
-import { faDigitalOcean } from "@fortawesome/free-brands-svg-icons";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "../../components/NavBar";
 import Footer from "../../components/Footer";
@@ -21,25 +10,49 @@ import HACKMERCED_TITLE from "../../assets/images/placeholder-title.png";
 import HACKMERCED_TOWER from "../../assets/images/tower.png";
 import TEAM_PICTURE from "../../assets/images/hackmerced-v.jpg";
 import team from "../../assets/team";
+import winners from "../../assets/winners";
 import "./styles.scss";
 
-const teampics = (team: Array<{ url: string; src: string }>): JSX.Element => {
-    const teamArray: Array<JSX.Element> = team.map((member: { url: string; src: string }, index: number) => {
-        const name: string = member.src.includes("/")
-            ? member.src.split("/")[3].split(".")[0]
-            : member.src.split(".")[0];
-        return (
-            <li key={index}>
-                <div className="hexagon">
-                    <a href={member.url} target="_blank" rel="noopener noreferrer">
-                        <img className="Picture" src={member.src} alt={name.charAt(0).toUpperCase() + name.slice(1)} />
-                    </a>
-                </div>
-            </li>
-        );
-    });
+type TeamProps = {
+    firstName?: string | undefined;
+    lastName?: string | undefined;
+    url: string;
+    src: string;
+};
+
+type WinnersProps = {
+    icon: IconDefinition;
+    category: string;
+    group: string;
+    project: string;
+};
+
+const generateTeamPics = (team: Array<TeamProps>): JSX.Element => {
+    const teamArray: Array<JSX.Element> = team.map((member: TeamProps, index: number) => (
+        <li key={index}>
+            <div className="hexagon">
+                <a href={member.url} target="_blank" rel="noopener noreferrer">
+                    <img className="Picture" src={member.src} alt={`${member.firstName} ${member.lastName}`} />
+                </a>
+            </div>
+        </li>
+    ));
 
     return <ul className="CenterHexagons">{teamArray}</ul>;
+};
+
+const generateWinners = (winners: Array<WinnersProps>): JSX.Element => {
+    const winnersArray: Array<JSX.Element> = winners.map((winner: WinnersProps, index: number) => (
+        <li key={index}>
+            <a href={winner.project} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={winner.icon} className="fa winner-icon" />
+                <h5 className="winner-category">{winner.category}</h5>
+                <h6 className="winner-group">{winner.group}</h6>
+            </a>
+        </li>
+    ));
+
+    return <ul className="winners">{winnersArray}</ul>;
 };
 
 const Home: FC = (): JSX.Element => {
@@ -48,26 +61,28 @@ const Home: FC = (): JSX.Element => {
     return (
         <main className="home">
             <Navbar />
-            <section className="TITLE">
+            <section className="TITLE home__heading">
                 {width > 700 ? (
-                    <section className="t-wave">
-                        <div className="cloud c1"></div>
-                        <div className="cloud c2"></div>
-                        <div className="cloud c3"></div>
-                        <div className="cloud c4"></div>
-                        <div className="cloud c5"></div>
+                    <section className="t-wave home__heading__wave">
+                        <div className="cloud home__heading__cloud c1"></div>
+                        <div className="cloud home__heading__cloud c2"></div>
+                        <div className="cloud home__heading__cloud c3"></div>
+                        <div className="cloud home__heading__cloud c4"></div>
+                        <div className="cloud home__heading__cloud c5"></div>
                     </section>
                 ) : null}
-                <section className="hackmerced">
+                <section className="hackmerced home_heading__title">
                     <img
                         src={HACKMERCED_TITLE}
                         width="400"
                         height="175"
                         alt="HACKMERCED TITLE"
-                        className="hackmerced-title"
+                        className="hackmerced-title home__heading__title__img"
                     />
-                    <div className="hackmerced-subtext text-gray-100">The biggest San Joaquin Valley hackathon.</div>
-                    <div className="hackmerced-text text-gray-100">
+                    <div className="hackmerced-text home__heading__title__subtext text-gray-100">
+                        The biggest San Joaquin Valley hackathon.
+                    </div>
+                    <div className="hackmerced-text home__heading__title__sub-text text-gray-100">
                         Stay updated with HackMerced and subscribe to our email list!
                     </div>
                 </section>
@@ -77,7 +92,7 @@ const Home: FC = (): JSX.Element => {
                         width="525"
                         height="1005"
                         alt="HACKMERCED TOWER"
-                        className="hackmerced-tower"
+                        className="hackmerced-tower home__heading__tower"
                     />
                 ) : null}
             </section>
@@ -103,65 +118,14 @@ const Home: FC = (): JSX.Element => {
             <section className="PAST-WINNERS">
                 <h3 className="past-winners-title">Past Winners</h3>
                 <div className="break-line"></div>
-                <nav className="winners">
-                    <a href="https://devpost.com/software/helpers-spot" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faHandHoldingHeart} className="fa winner-icon" />
-                        <h5 className="winner-title">Social Good</h5>
-                        <h6 className="winner-group">Sustainably Yours</h6>
-                    </a>
-                    <a href="https://devpost.com/software/vibe-xja5cd" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faHandshake} className="fa winner-icon" />
-                        <h5 className="winner-title">Inclusivity</h5>
-                        <h6 className="winner-group">Undyslexify</h6>
-                    </a>
-                    <a href="https://devpost.com/software/kabobcat" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faLaptopCode} className="fa winner-icon" />
-                        <h5 className="winner-title">CyberSecurity</h5>
-                        <h6 className="winner-group">Repul.ink</h6>
-                    </a>
-                    <a href="https://devpost.com/software/code-dude" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faPalette} className="fa winner-icon" />
-                        <h5 className="winner-title">Best in Design</h5>
-                        <h6 className="winner-group">Sustainably Yours</h6>
-                    </a>
-                    <a href="https://devpost.com/software/snapcode-vabpfn" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faSchool} className="fa winner-icon" />
-                        <h5 className="winner-title">Best UC Merced-Related Hack</h5>
-                        <h6 className="winner-group">Snapcode</h6>
-                    </a>
-                    <a href="https://devpost.com/software/esose" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faSearch} className="fa winner-icon" />
-                        <h5 className="winner-title">Tech for Social Good</h5>
-                        <h6 className="winner-group">Vacchecker</h6>
-                    </a>
-                    <a href="https://devpost.com/software/3d-for-education" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faMicrochip} className="fa winner-icon" />
-                        <h5 className="winner-title">Best Hardware Hack</h5>
-                        <h6 className="winner-group">EcoSafe</h6>
-                    </a>
-                    <a href="https://devpost.com/software/ghym" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faMeteor} className="fa winner-icon" />
-                        <h5 className="winner-title">Moonshot</h5>
-                        <h6 className="winner-group">Blood Buds</h6>
-                    </a>
-                    <a href="https://devpost.com/software/a-clear-space" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faDigitalOcean} className="fa winner-icon" />
-                        <h5 className="winner-title">Best Domain Using Domain.com</h5>
-                        <h6 className="winner-group">StreetSpots.Online</h6>
-                    </a>
-                    <a href="https://devpost.com/software/project-dms" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faCloud} className="fa winner-icon" />
-                        <h5 className="winner-title">Best Use of Google Cloud</h5>
-                        <h6 className="winner-group">IdVision</h6>
-                    </a>
-                </nav>
+                {generateWinners(winners)}
             </section>
             <section className="home__team-pictures">
                 <header className="Header">
                     <h1>Our Team</h1>
                     <div className="break-line"></div>
                 </header>
-                <div className="home__team-pictures grid clear">{teampics(team)}</div>
+                <div className="home__team-pictures grid clear">{generateTeamPics(team)}</div>
             </section>
             <Footer />
         </main>
