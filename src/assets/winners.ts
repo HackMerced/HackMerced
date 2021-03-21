a6218122efb87e24a776fa11b6f735af379ae0d9
Adrian Darian <adarian@ucmerced.edu>
2021-03-21 14:45:35 -0700

    fix: winners to be dynamic and added some new classNames

---
+++ src/assets/winners.ts
@@ -0,0 +1,77 @@
+import {
+    faHandHoldingHeart,
+    faHandshake,
+    faLaptopCode,
+    faPalette,
+    faMeteor,
+    faSchool,
+    faMicrochip,
+    faSearch,
+    faCloud,
+} from "@fortawesome/free-solid-svg-icons";
+import { faDigitalOcean } from "@fortawesome/free-brands-svg-icons";
+
+const winners = [
+    {
+        icon: faHandHoldingHeart,
+        category: "Social Good",
+        group: "Sustainably Yours",
+        project: "https://devpost.com/software/helpers-spot",
+    },
+    {
+        icon: faHandshake,
+        category: "Inclusivity",
+        group: "Undyslexify",
+        project: "https://devpost.com/software/vibe-xja5cd",
+    },
+    {
+        icon: faLaptopCode,
+        category: "CyberSecurity",
+        group: "Repul.ink",
+        project: "https://devpost.com/software/kabobcat",
+    },
+    {
+        icon: faPalette,
+        category: "Best in Design",
+        group: "Sustainably Yours",
+        project: "https://devpost.com/software/code-dude",
+    },
+    {
+        icon: faSchool,
+        category: "Best UC Merced-Related Hack",
+        group: "Snapcode",
+        project: "https://devpost.com/software/snapcode-vabpfn",
+    },
+    {
+        icon: faSearch,
+        category: "Tech for Social Good",
+        group: "Vacchecker",
+        project: "https://devpost.com/software/esose",
+    },
+    {
+        icon: faMicrochip,
+        category: "Best Hardware Hack",
+        group: "EcoSafe",
+        project: "https://devpost.com/software/3d-for-education",
+    },
+    {
+        icon: faMeteor,
+        category: "Moonshot",
+        group: "Blood Buds",
+        project: "https://devpost.com/software/ghym",
+    },
+    {
+        icon: faDigitalOcean,
+        category: "Best Domain Using Domain.com",
+        group: "StreetSpots.Online",
+        project: "https://devpost.com/software/a-clear-space",
+    },
+    {
+        icon: faCloud,
+        category: "Best Use of Google Cloud",
+        group: "IdVision",
+        project: "https://devpost.com/software/project-dms",
+    },
+];
+
+export default winners;
