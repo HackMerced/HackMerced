6f37fdce6c3604b6b813e4902258cff377c623c7
Adrian Darian <adarian@ucmerced.edu>
2021-03-21 15:51:06 -0700

    fix: naming conventions in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -29,12 +29,19 @@ type WinnersProps = {
 
 const generateTeamPics = (team: Array<TeamProps>): JSX.Element => {
     const teamArray: Array<JSX.Element> = team.map((member: TeamProps, index: number) => (
-        <li key={index}>
-            <div className="hexagon">
-                <a href={member.url} target="_blank" rel="noopener noreferrer">
-                    <img className="Picture" src={member.src} alt={`${member.firstName} ${member.lastName}`} />
-                </a>
-            </div>
+        <li key={index} className="home__team-pictures__grid__items">
+            <a
+                href={member.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="home__team-pictures__grid__items__hexagon"
+            >
+                <img
+                    className="home__team-pictures__grid__items__hexagon__picture"
+                    src={member.src}
+                    alt={`${member.firstName} ${member.lastName}`}
+                />
+            </a>
         </li>
     ));
 
@@ -43,16 +50,25 @@ const generateTeamPics = (team: Array<TeamProps>): JSX.Element => {
 
 const generateWinners = (winners: Array<WinnersProps>): JSX.Element => {
     const winnersArray: Array<JSX.Element> = winners.map((winner: WinnersProps, index: number) => (
-        <li key={index}>
+        <li key={index} className="home__past-winners__list__past-winner">
             <a href={winner.project} target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={winner.icon} className="fa winner-icon" />
-                <h5 className="winner-category">{winner.category}</h5>
-                <h6 className="winner-group">{winner.group}</h6>
+                <FontAwesomeIcon icon={winner.icon} className="fa home__past-winners__list__past-winner__icon" />
+                <h5 className="home__past-winners__list__past-winner__category">{winner.category}</h5>
+                <h6 className="home__past-winners__list__past-winner__group">{winner.group}</h6>
             </a>
         </li>
     ));
 
-    return <ul className="winners">{winnersArray}</ul>;
+    return <ul className="home__past-winners__list">{winnersArray}</ul>;
+};
+
+const generateSectionHeading = (title: string): JSX.Element => {
+    return (
+        <>
+            <h1 className="home__section-title">{title}</h1>
+            <div className="home__break-line"></div>
+        </>
+    );
 };
 
 const Home: FC = (): JSX.Element => {
@@ -61,28 +77,28 @@ const Home: FC = (): JSX.Element => {
     return (
         <main className="home">
             <Navbar />
-            <section className="TITLE home__heading">
+            <section className="home__heading">
                 {width > 700 ? (
-                    <section className="t-wave home__heading__wave">
-                        <div className="cloud home__heading__cloud c1"></div>
-                        <div className="cloud home__heading__cloud c2"></div>
-                        <div className="cloud home__heading__cloud c3"></div>
-                        <div className="cloud home__heading__cloud c4"></div>
-                        <div className="cloud home__heading__cloud c5"></div>
+                    <section className="home__heading__wave">
+                        <div className="home__heading__wave__cloud c1"></div>
+                        <div className="home__heading__wave__cloud c2"></div>
+                        <div className="home__heading__wave__cloud c3"></div>
+                        <div className="home__heading__wave__cloud c4"></div>
+                        <div className="home__heading__wave__cloud c5"></div>
                     </section>
                 ) : null}
-                <section className="hackmerced home_heading__title">
+                <section className="home__heading__title">
                     <img
                         src={HACKMERCED_TITLE}
                         width="400"
                         height="175"
-                        alt="HACKMERCED TITLE"
-                        className="hackmerced-title home__heading__title__img"
+                        alt="HackMerced Title Image"
+                        className="home__heading__title__img"
                     />
-                    <div className="hackmerced-text home__heading__title__subtext text-gray-100">
+                    <div className="home__heading__title__text text-gray-100">
                         The biggest San Joaquin Valley hackathon.
                     </div>
-                    <div className="hackmerced-text home__heading__title__sub-text text-gray-100">
+                    <div className="home__heading__title__sub-text text-gray-100">
                         Stay updated with HackMerced and subscribe to our email list!
                     </div>
                 </section>
@@ -91,16 +107,15 @@ const Home: FC = (): JSX.Element => {
                         src={HACKMERCED_TOWER}
                         width="525"
                         height="1005"
-                        alt="HACKMERCED TOWER"
-                        className="hackmerced-tower home__heading__tower"
+                        alt="HackMerced Tower"
+                        className="home__heading__tower"
                     />
                 ) : null}
             </section>
-            <section className="about-us">
-                <h1 className="about-us__title">About Us</h1>
-                <div className="about-us__break-line"></div>
-                <section className="about-us__content">
-                    <article className="about-us__content__description">
+            <section className="home__about-us">
+                {generateSectionHeading("About Us")}
+                <section className="home__about-us__content">
+                    <article className="home__about-us__content__description">
                         <p>
                             HackMerced is a 36 hour annual programming competition that occurs at the University of
                             California, Merced and is open to students from all over the world. During the event,
@@ -112,20 +127,16 @@ const Home: FC = (): JSX.Element => {
                             within the San Joaquin Valley.
                         </p>
                     </article>
-                    <img className="about-us__content__team-picture" src={TEAM_PICTURE} alt="HackMerced Team" />
+                    <img className="home__about-us__content__team-picture" src={TEAM_PICTURE} alt="HackMerced Team" />
                 </section>
             </section>
-            <section className="PAST-WINNERS">
-                <h3 className="past-winners-title">Past Winners</h3>
-                <div className="break-line"></div>
+            <section className="home__past-winners">
+                {generateSectionHeading("Past Winners")}
                 {generateWinners(winners)}
             </section>
             <section className="home__team-pictures">
-                <header className="Header">
-                    <h1>Our Team</h1>
-                    <div className="break-line"></div>
-                </header>
-                <div className="home__team-pictures grid clear">{generateTeamPics(team)}</div>
+                {generateSectionHeading("Our Team")}
+                <div className="home__team-pictures__grid clear">{generateTeamPics(team)}</div>
             </section>
             <Footer />
         </main>
