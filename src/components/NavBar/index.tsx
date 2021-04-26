744bb7b974f27f2f383230f7764e724ded234f94
Abdus Samee <abdussamee16@gmail.com>
2021-04-26 22:20:38 +0600

    Changed the navbar colour
    Navbar colour changes in mobile view. The colours in dark and light themes are different.

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -41,6 +41,7 @@ const Navbar: FC<{ backgroundColor?: string; textColor?: string; breakLineColor?
 
     const NavbarItemsProps = {
         openDrawer: openDrawer,
+        theme: theme,
     };
 
     // create a break line
@@ -156,7 +157,7 @@ const Menu = {
             padding: 0 2rem 1rem 2rem;
             margin-top: 0;
             transition: 0.2s ease-out;
-            background-color: #ffb181;
+            background-color: ${() => (document.documentElement.getAttribute("data-theme") === "dark" ? "#ffb26e" : "rgb(184, 61, 24)")};
             z-index: 101;
             transform: ${({ openDrawer }: { openDrawer: any }) => (openDrawer ? `translateX(0)` : `translateX(100%)`)};
         }
