2f6d1967467a9db759d960bb538d66a4aa782688
shaktiv29 <shaktiv@jecjabalpur.ac.in>
2021-06-23 23:14:17 +0530

    Updating Sponsor Section

--- src/pages/SponsorUs/index.tsx
+++ src/pages/SponsorUs/index.tsx
@@ -6,6 +6,19 @@ import Navbar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import Form from "../../components/Form";
 // import PAST_SPONSORS from "../../assets/images/past-sponsors.png";
+import A from "../../assets/images/sponsor-logos/1.png";
+import B from "../../assets/images/sponsor-logos/2.png";
+import C from "../../assets/images/sponsor-logos/3.png";
+import D from "../../assets/images/sponsor-logos/4.png";
+import E from "../../assets/images/sponsor-logos/5.png";
+import F from "../../assets/images/sponsor-logos/6.png";
+import G from "../../assets/images/sponsor-logos/7.png";
+import H from "../../assets/images/sponsor-logos/8.png";
+import I from "../../assets/images/sponsor-logos/9.png";
+import J from "../../assets/images/sponsor-logos/10.png";
+import K from "../../assets/images/sponsor-logos/11.png";
+import L from "../../assets/images/sponsor-logos/12.png";
+
 
 import "./styles.scss";
 
@@ -59,6 +72,23 @@ const SponsorUs: FC = (): JSX.Element => {
                         className="sponsors-us__past-sponsors__img"
                     />
                 </section> */}
+                <div id="past-sponsors">
+                    Our Past Sponsors
+                    <ul className="listt">
+                        <li className="items"><img src={A} /></li>
+                        <li className="items"><img src={B} /></li>
+                        <li className="items"><img src={C} /></li>
+                        <li className="items"><img src={D} /></li>
+                        <li className="items"><img src={E} /></li>
+                        <li className="items"><img src={F} /></li>
+                        <li className="items"><img src={G} /></li>
+                        <li className="items"><img src={H} /></li>
+                        <li className="items"><img src={I} /></li>
+                        <li className="items"><img src={J} /></li>
+                        <li className="items"><img src={K} /></li>
+                        <li className="items"><img src={L} /></li>
+                    </ul>
+                </div>
             </main>
             <Footer />
         </Fragment>
