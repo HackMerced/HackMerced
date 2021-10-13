dc6e969eea274be339b47308cec4564e0c2c557d
Abdus Samee <abdussamee16@gmail.com>
2021-10-13 20:50:35 +0600

    Added timeline with demo events

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -4,44 +4,37 @@ import "./styles.scss";
 
 const Table: FC<{}> = (): JSX.Element => {
     return(
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Sl. No.</th>
-                        <th>Time</th>
-                        <th>Event</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                       <td>1</td>
-                       <td>1:00 pm</td>
-                       <td>Opening Ceremony</td> 
-                    </tr>
-                    <tr>
-                       <td>2</td>
-                       <td>2:00 pm</td>
-                       <td>Hackathon Starts</td> 
-                    </tr>
-                    <tr>
-                       <td>3</td>
-                       <td>2:30 pm</td>
-                       <td>Break</td> 
-                    </tr>
-                    <tr>
-                       <td>4</td>
-                       <td>3:30 pm</td>
-                       <td>Hackathon Resumes</td> 
-                    </tr>
-                    <tr>
-                       <td>5</td>
-                       <td>5:00 pm</td>
-                       <td>Closing ceremony</td> 
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <>
+            <div className="heading">
+                <h2 className="heading-text">Schedule</h2>
+            </div>
+            <div className="timeline">
+                <div className="container left">
+                    <div className="content">
+                    <span className="title">12:30 pm</span>
+                    <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per.</p>
+                    </div>
+                </div>
+                <div className="container right">
+                    <div className="content">
+                    <span className="title">1:00 pm</span>
+                    <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per.</p>
+                    </div>
+                </div>
+                <div className="container left">
+                    <div className="content">
+                    <span className="title">1:30 pm</span>
+                    <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per.</p>
+                    </div>
+                </div>
+                <div className="container right">
+                    <div className="content">
+                    <span className="title">2:00 pm</span>
+                    <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per.</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
