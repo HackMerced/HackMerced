e6783b1d8c3550269dd5dfef73dded91bfd18b4a
Betsy <b_avila@outlook.com>
2021-10-13 22:51:52 -0700

    created link in button for typeform

--- src/pages/LearnDay/index.tsx
+++ src/pages/LearnDay/index.tsx
@@ -5,24 +5,6 @@ import Navbar from "../../components/NavBar";
 import "./styles.scss";
 
 const LearnDay: FC = (): JSX.Element => {
-    // const { width } = useWindowDimensions();
-    // const [form, setForm] = useState<{ email: string }>({ email: "" });
-
-    // // Handles Change on the fields of the form
-    // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    //     const { name, value } = event.target;
-    //     setForm({ ...form, [name]: value });
-    // };
-
-    // // Handles the submission action when the submit button is pressed
-    // const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>): void => {
-    //     event.preventDefault();
-    //     Axios.post(
-    //         `https://hackmerced-tomoe.herokuapp.com/v1/auth/login-resetpassword`,
-    //         form,
-    //     ).then((response: AxiosResponse) => console.log(response));
-    // };
-
     return (
         /*Title component*/
         <main>
@@ -32,7 +14,7 @@ const LearnDay: FC = (): JSX.Element => {
                     <div className="title">Hacktually II</div>
                     <div className="h2">Local Hack Day: Learn</div>
                     <div className="h3">Learn to tackle new skills, explore different realms of technology, and champion exciting projects in a workshop event hosted by HackMerced on <strong>November 13, 2021.</strong></div>
-                    <button className="ld_button">Sign Up</button>
+                    <a target="_blank" href="linkaddress"><button className="ld_button">Sign Up</button></a>
                 </div>
                 <div className="images"></div>
             </section>
