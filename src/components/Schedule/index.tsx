5d7b5666b84e6af25f894acad28843ccf35fbeb6
Mary <mdelosreyes3@ucmerced.edu>
2021-10-18 14:46:29 -0700

    fix: schedule style and content

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -12,13 +12,26 @@ const Table: FC<any> = (): JSX.Element => {
                 <div className="container left">
                     <div className="content">
                         <span className="title">12:30 PM PST</span>
-                        <p>Check-in</p>
+                        <p
+                            className="checkin"
+                            data-tip="Location: Room 130 (in-person only)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
+                            Check-in
+                        </p>
                     </div>
                 </div>
                 <div className="container right">
                     <div className="content">
                         <span className="title">1:00 PM PST</span>
-                        <p className="opening" data-tip="Introduction - Hackmerced" style={{ position: "relative" }}>
+                        <p
+                            className="opening"
+                            data-tip="
+                            Location: Room 130 (in-person)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
                             Opening Ceremony
                         </p>
                     </div>
@@ -26,16 +39,23 @@ const Table: FC<any> = (): JSX.Element => {
                 <div className="container left">
                     <div className="content">
                         <span className="title">1:30-2:30 PM PST</span>
-                        <p className="lightChallenge" data-tip="Room - 140" style={{ position: "relative" }}>
+                        <p
+                            className="lightChallenge"
+                            data-tip="Location: Room 140 (in-person)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
                             Light Challenge
                         </p>
-                        <p className="design" data-tip="Room - 130" style={{ position: "relative" }}>
+                        <p
+                            className="design"
+                            data-tip="Location: Room 130 (in-person)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
                             Design Workshop
                         </p>
-                        <p className="charades" data-tip="Room - 110" style={{ position: "relative" }}>
-                            Charades
-                        </p>
-                        <p className="lightChallenge" data-tip="Twitch" style={{ position: "relative" }}>
+                        <p className="lightChallenge" data-tip="Online: Twitch" style={{ position: "relative" }}>
                             Marble Race
                         </p>
                     </div>
@@ -43,51 +63,60 @@ const Table: FC<any> = (): JSX.Element => {
                 <div className="container right">
                     <div className="content">
                         <span className="title">2:30-3:30 PM PST</span>
-                        <p className="github" data-tip="Room - 130" style={{ position: "relative" }}>
-                            Code Collab w/ GitHub
-                        </p>
                         <p
-                            className="games"
-                            data-tip="Among Us - online Red light,Green light - Outside Room - 110"
+                            className="github"
+                            data-tip="Location: Room 130 (in-person)
+                            Online: Discord"
                             style={{ position: "relative" }}
                         >
-                            Among Us, Red light & Green light
+                            Code Collab w/ GitHub
+                        </p>
+                        <p className="games" data-tip="Online: Discord" style={{ position: "relative" }}>
+                            Among Us
                         </p>
                     </div>
                 </div>
                 <div className="container left">
                     <div className="content">
                         <span className="title">2:30-4:30 PM PST</span>
-                        <p className="acm" data-tip="Room - 140" style={{ position: "relative" }}>
-                            Hack the Power collab with SEA and ACM
+                        <p
+                            className="acm"
+                            data-tip="Location: Room 140 (in-person)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
+                            Hack the Power with SEA and ACM
                         </p>
                     </div>
                 </div>
                 <div className="container right">
                     <div className="content">
                         <span className="title">3:30-4:30 PM PST</span>
-                        <p className="notion" data-tip="Room - 130" style={{ position: "relative" }}>
-                            Project Management w/ Notion
-                        </p>
                         <p
-                            className="gartic"
-                            data-tip="Garticphone(Online) Karaoke Slideshow Room - 110"
+                            className="notion"
+                            data-tip="Location: Room 130 (in-person)
+                            Online: Discord"
                             style={{ position: "relative" }}
                         >
-                            Garticphone and Karaoke slideshow
+                            Project Management w/ Notion
+                        </p>
+                        <p className="gartic" data-tip="Online: Discord" style={{ position: "relative" }}>
+                            Garticphone
                         </p>
                     </div>
                 </div>
                 <div className="container left">
                     <div className="content">
                         <span className="title">4:30-5:30 PM PST</span>
-                        <p className="interview" data-tip="Room - 130" style={{ position: "relative" }}>
+                        <p
+                            className="interview"
+                            data-tip="Location: Room 130 (in-person)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
                             Hack the Technical Interview
                         </p>
-                        <p className="chalk" data-tip="Outside" style={{ position: "relative" }}>
-                            Chalk competition
-                        </p>
-                        <p className="typing" data-tip="Online" style={{ position: "relative" }}>
+                        <p className="typing" data-tip="Online: Discord" style={{ position: "relative" }}>
                             Typing speed race
                         </p>
                     </div>
@@ -95,7 +124,13 @@ const Table: FC<any> = (): JSX.Element => {
                 <div className="container right">
                     <div className="content">
                         <span className="title">5:30-6:30 PM PST</span>
-                        <p>Dinner</p>
+                        <p
+                            className="opening"
+                            data-tip="Location: Room 130 (in-person only)"
+                            style={{ position: "relative" }}
+                        >
+                            Dinner
+                        </p>
                     </div>
                 </div>
                 <div className="container left">
@@ -107,7 +142,14 @@ const Table: FC<any> = (): JSX.Element => {
                 <div className="container right">
                     <div className="content">
                         <span className="title">7:00 PM PST</span>
-                        <p>Closing Ceremony</p>
+                        <p
+                            className="opening"
+                            data-tip="Location: Room 130 (in-person)
+                            Online: Discord"
+                            style={{ position: "relative" }}
+                        >
+                            Closing Ceremony
+                        </p>
                     </div>
                 </div>
             </div>
