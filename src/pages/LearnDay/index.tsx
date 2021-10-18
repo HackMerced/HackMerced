2c5b2a5f787156eaeac52e130b4cf4d16db69e4d
Mary <mdelosreyes3@ucmerced.edu>
2021-10-18 14:48:27 -0700

    fix: lhd title style

--- src/pages/LearnDay/index.tsx
+++ src/pages/LearnDay/index.tsx
@@ -11,6 +11,24 @@ const LearnDay: FC = (): JSX.Element => {
     return (
         <main className="LearnDay">
             <Navbar />
+            <section className="grid-container">
+                <div className="texts">
+                    <div className="hacktually-title">Hacktually II</div>
+                    <div className="h2">Local Hack Day: Learn</div>
+                    <div className="h3">
+                        Learn to tackle new skills, explore different realms of technology, and champion exciting
+                        projects in a workshop, hybrid event hosted by HackMerced on <strong>November 13, 2021.</strong>
+                    </div>
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://hackmerced2021.typeform.com/locallearnday"
+                    >
+                        <button className="ld_button">Apply Now</button>
+                    </a>
+                </div>
+                <div className="images"></div>
+            </section>
             <Schedule />
             <FAQ />
             <Footer />
