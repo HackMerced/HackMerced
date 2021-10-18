48298850c89519952fccbcd59806fd0c31c748a7
Adrian Darian <adarian@ucmerced.edu>
2021-10-18 01:09:20 -0700

    patch up rebase

--- src/pages/LearnDay/index.tsx
+++ src/pages/LearnDay/index.tsx
@@ -11,6 +11,18 @@ const LearnDay: FC = (): JSX.Element => {
     return (
         <main className="LearnDay">
             <Navbar />
+            <section className="grid-container">
+                <div className="texts">
+                    <div className="title">Hacktually II</div>
+                    <div className="h2">Local Hack Day: Learn</div>
+                    <div className="h3">
+                        Learn to tackle new skills, explore different realms of technology, and champion exciting
+                        projects in a workshop event hosted by HackMerced on <strong>November 13, 2021.</strong>
+                    </div>
+                    <button className="ld_button">Sign Up</button>
+                </div>
+                <div className="images"></div>
+            </section>
             <Schedule />
             <FAQ />
             <Footer />
