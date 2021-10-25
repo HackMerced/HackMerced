123f428bd99502ca5287c657d239f835dd0d14a5
Mary <mdelosreyes3@ucmerced.edu>
2021-10-25 13:58:42 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e627edeb967ac1b303739ff833e30ad3.js"
+  "/precache-manifest.8b88e4fc0c7c3cea7f505d17155510c0.js"
 );
 
 self.addEventListener('message', (event) => {
