b93d955f3fed0804eb6f5715404485ad956bef26
maryharebear <55856581+maryharebear@users.noreply.github.com>
2022-03-09 00:05:49 +0000

    Deploying to gh-pages from  @ 935031c4bf9849862cc3676a93aa806fa6d17564 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6802b89e9435b8fa94ec85be28cccca2.js"
+  "/precache-manifest.c5595df029e57610eab013671ef784b3.js"
 );
 
 self.addEventListener('message', (event) => {
